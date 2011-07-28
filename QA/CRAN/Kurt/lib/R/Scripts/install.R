022229f6355af94e94d3fc7322889a7084673538
hornik <hornik@c52295ea-58df-0310-926a-d16021944841>
2011-07-28 14:29:08 +0000

    Added.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2516 c52295ea-58df-0310-926a-d16021944841

---
+++ QA/CRAN/Kurt/lib/R/Scripts/install.R
@@ -0,0 +1,19 @@
+## Argument handling.
+args <- commandArgs(trailingOnly = TRUE)
+args <- gsub("_.*", "", basename(args))
+
+## Now build a package db from the source packages in the working
+## directory (but only if there is none already).
+if(file.exists("PACKAGES")
+   || !length(list.files(pattern = "\\.tar\\.gz$"))) {
+    dir <- NULL
+} else {
+    dir <- getwd()
+    tools::write_PACKAGES(dir)
+    ## on.exit(unlink(file.path(dir, c("PACKAGES", "PACKAGES.gz"))))
+    dir <- sprintf("file://%s", dir)
+}
+
+for(a in args)
+    install.packages(a, lib = .libPaths()[1L], dependencies = TRUE,
+                     contriburl = c(dir, contrib.url(getOption("repos"))))
