f52b993ac6c3b02dbb3458a3fc013bd9d62773ea
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2012-01-21 08:30:21 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2606 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Windows/tests/exceptions.R
+++ CRAN/QA/BDR/Windows/tests/exceptions.R
@@ -8,7 +8,7 @@ stoplist <-
       "ROracle", "Rcplex", "RScaLAPACK",
       "Rlof", "Rmosek",  "SV", "cplexAPI", "mpc", "mvgraph", "rzmq", "udunits2",
       "ncdf4", "M3", # ncdf4
-      "Rsymphony", "ROI.plugin.symphony",
+      # "Rsymphony", "ROI.plugin.symphony",
       "CARramps", "cudaBayesreg", "gputools", "magma", # cuda
       "IQMNMR", "Rmpi", "bcool", "doMPI", "rpvm", "npRmpi", "rpud", "sprint", # mpi
 # Unix-only
@@ -25,18 +25,19 @@ biarch <- c("SQLiteMap", "arulesSequences", "clpAPI", "glpkAPI") # my update of 
 
 multi <- c("Cairo", "Cubist", "FastRWeb", "GWAtoolbox", "JavaGD", "RCurl",
            "RInside", "RJSONIO", "RMySQL", "RPostgreSQL", "Rserve",
-           "Rssa", "bigmemory", "maps", "mvabund", "rJava", "rcom", "rgl")
+           "Rssa", "bigmemory", "maps", "rJava", "rcom", "rgl")
 
-extras <- c("SSOAP", "XMLRPC", "XMLSchema", "yags")
+extras <- c("XMLRPC", "yags")
 
-ggobi_users <- c("rggobi", "gcmrec", "magnets", "WMTregions", "beadarrayMSV",
-                 "clusterfly", "PKgraph")
+ggobi_users <- c("rggobi",  "PKgraph", "WMTregions", "beadarrayMSV",
+                 "clusterfly", "magnets")
 
 if(getRversion() < "2.15.0") {
     nomulti <- c(ggobi_users,
                  "OSACC", # fail to compile, tries to store pointers in long
                  "RSvgDevice", "RSVGTipsDevice", "eco") # crashes
-    stoplist <- c(stoplist, "clpAPI", "glpkAPI", "RQuantLib")
+    stoplist <- c(stoplist, "Rsymphony", "ROI.plugin.symphony",
+                  "clpAPI", "glpkAPI", "RQuantLib")
 } else {
     stoplist <- c(stoplist,
                   "h5r",
