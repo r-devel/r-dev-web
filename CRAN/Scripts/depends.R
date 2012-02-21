baa2bf961bbe23f8c6d427cb8fd27043430b8f42
hornik <hornik@c52295ea-58df-0310-926a-d16021944841>
2012-02-21 09:36:27 +0000

    Added.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2638 c52295ea-58df-0310-926a-d16021944841

---
+++ CRAN/Scripts/depends.R
@@ -0,0 +1,24 @@
+require("tools")
+
+reverse_dependencies_with_maintainers <-
+function(packages, which = c("Depends", "Imports", "LinkingTo"),
+         recursive = FALSE)
+{
+    description <- sprintf("%s/web/packages/packages.rds",
+                           getOption("repos")["CRAN"])
+    con <- if(substring(description, 1L, 7L) == "file://")
+        file(description, "rb")
+    else
+        url(description, "rb")
+    on.exit(close(con))
+    db <- readRDS(gzcon(con))
+    rownames(db) <- NULL
+    
+    rdepends <- package_dependencies(packages, db, which,
+                                     recursive = recursive,
+                                     reverse = TRUE)
+    rdepends <- sort(unique(unlist(rdepends)))
+    pos <- match(rdepends, db[, "Package"], nomatch = 0L)
+
+    db[pos, c("Package", "Version", "Maintainer")]
+}
