f8bd90af1f35524b84a5da8df2b0a67ca7a25bf8
ligges <ligges@c52295ea-58df-0310-926a-d16021944841>
2012-05-11 12:47:35 +0000

    update for 2.16
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2685 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/Uwe/make/check_diffs_send.R
+++ CRAN/QA/Uwe/make/check_diffs_send.R
@@ -2,8 +2,10 @@ source("d:/Rcompile/CRANpkg/make/check_diffs.R")
 
 save_results("2.14")
 save_results("2.15")
+save_results("2.16")
 try(check_results_diffs("2.14"))
 try(check_results_diffs("2.15"))
+try(check_results_diffs("2.16"))
 
 shell(paste("blat d:\\Rcompile\\CRANpkg\\win\\2.14\\stats\\checkdiff-", Sys.Date(), "-", Sys.Date()-1, ".txt ", 
     "-to ligges@statistik.tu-dortmund.de -subject checkdiffs_2.14_", 
@@ -11,9 +13,6 @@ shell(paste("blat d:\\Rcompile\\CRANpkg\\win\\2.14\\stats\\checkdiff-", Sys.Date
 shell(paste("blat d:\\Rcompile\\CRANpkg\\win\\2.15\\stats\\checkdiff-", Sys.Date(), "-", Sys.Date()-1, ".txt ", 
     "-to ligges@statistik.tu-dortmund.de", if(Sys.getenv("Kurt") == "Kurt") " -cc Kurt.Hornik@R-Project.org,Martin.Maechler@R-project.org", " -subject checkdiffs_2.15_", 
     "svn_", R.version[["svn rev"]], "_", Sys.Date()-1, "_", Sys.Date(), " -f ligges@statistik.tu-dortmund.de", sep=""))
-
-
-#check_results_diffs("2.11", date.old = Sys.Date()-6)
-#shell(paste("blat d:\\Rcompile\\CRANpkg\\win\\2.11\\stats\\checkdiff-", Sys.Date(), "-", Sys.Date()-6, ".txt ", 
-#    "-to ligges@statistik.tu-dortmund.de", if(Sys.getenv("Kurt") == "Kurt") " -cc Kurt.Hornik@R-Project.org,Martin.Maechler@R-project.org", " -subject checkdiffs_2.11_", 
-#    "svn_", R.version[["svn rev"]], "_", Sys.Date()-6, "_", Sys.Date(), " -f ligges@statistik.tu-dortmund.de", sep=""))
+shell(paste("blat d:\\Rcompile\\CRANpkg\\win\\2.16\\stats\\checkdiff-", Sys.Date(), "-", Sys.Date()-1, ".txt ", 
+    "-to ligges@statistik.tu-dortmund.de -subject checkdiffs_2.16_", 
+    Sys.Date()-1, "_", Sys.Date(), " -f ligges@statistik.tu-dortmund.de", sep=""))
