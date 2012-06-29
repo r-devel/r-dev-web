37e44448173566913d3c7950c8b9c9a56767e3e0
hornik <hornik@c52295ea-58df-0310-926a-d16021944841>
2012-06-29 12:59:11 +0000

    Removed.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2703 c52295ea-58df-0310-926a-d16021944841

--- QA/CRAN/Kurt/lib/R/Scripts/check_profile.R
+++
@@ -1,8 +0,0 @@
-if(getRversion() >= "2.7.0") {
-    setHook(packageEvent("grDevices", "onLoad"),
-            function(...) {
-                grDevices::X11.options(type = "Cairo")
-            })
-} else {
-    options(X11colortype = "pseudo.cube")
-}
