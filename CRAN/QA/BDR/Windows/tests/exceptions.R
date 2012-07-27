e5d77a0bc8e3833e6703445020a738264e906dba
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2012-07-27 10:42:23 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2721 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Windows/tests/exceptions.R
+++ CRAN/QA/BDR/Windows/tests/exceptions.R
@@ -5,8 +5,8 @@ stoplist <-
       "rpart", "spatial", "survival",
 # Missing external software
       "GDD", "OpenCL", "RDF","RDieHarder", "RMongo", "ROAuth", "ROracle",
-      "Rcplex", "RScaLAPACK", "Rlof", "Rmosek",  "SV", "cplexAPI", "h5r",
-      "mimR", "mvgraph", "rpvm", "rzmq",
+      "Rcplex", "RScaLAPACK", "Rlof", "Rmosek", "cplexAPI", "h5r",
+      "mimR", "mvgraph", "rzmq",
       "ncdf4", "M3", # ncdf4
       "CARramps", "WideLM", "cudaBayesreg", "gputools", "magma", # cuda
       "qtbase", "qtpaint", "qtutils", "ProgGUIinR",
@@ -15,21 +15,20 @@ stoplist <-
       "R4dfp", "RBerkeley", "ROracleUI", "RProtoBuf",  "TSpadi", "VBmix",
       "WINRPACK", "cit", "cmprskContin", "doMC", "fork", "gcbd",
       "interactivity", "makesweave", "mfr", "multic", "multicore", "nice",
-      "parmigene", "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr",
+      "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr",
 # don't work
       "excel.link", # RDCOMClient
       "RWinEdt", # needs Rgui
       "rJavax", # horrible Java things
       ## and for 32-bit-only
-      # "rggobi",  "PKgraph",  "beadarrayMSV",
       "hdf5", "satin"
       )
 
 biarch <- c("SQLiteMap", "arulesSequences", "sparsenet", "udunits2")
 
-multi <- c("BayesXsrc", "Cairo", "Cubist", "EcoHydRology", "FastRWeb",
-           "GWAtoolbox", "JavaGD", "RCurl", "RInside", "RJSONIO",
-           "RMySQL", "RPostgreSQL", "Rserve", "Rssa", "SWATmodel",
-           "bigmemory", "maps", "rJava", "rcom", "rgl")
+multi <- c("BayesXsrc", "Cairo", "Cubist", "FastRWeb", "GWAtoolbox",
+           "JavaGD", "RCurl", "RInside", "RJSONIO", "RMySQL",
+           "RPostgreSQL", "Rserve", "Rssa", "SWATmodel", "bigmemory",
+           "maps", "rJava", "rcom", "rgl")
 
 extras <- c("XMLRPC", "yags")
