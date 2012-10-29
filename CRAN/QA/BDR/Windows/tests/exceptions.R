fae4a85470c3f1ee1b59e2f882167226891393c4
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2012-10-29 08:39:32 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2746 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Windows/tests/exceptions.R
+++ CRAN/QA/BDR/Windows/tests/exceptions.R
@@ -10,22 +10,26 @@ stoplist <-
       "ncdf4", "M3", # ncdf4
       "CARramps", "WideLM", "cudaBayesreg", "gputools", "magma", # cuda
       "qtbase", "qtpaint", "qtutils", "ProgGUIinR",
-      "IQMNMR", "Rmpi", "bcool", "doMPI", "npRmpi", "parspatstat", "pbdMPI", "pmclust", "rpud", "sprint", # mpi
+      "IQMNMR", "Rmpi", "bcool", "doMPI", "npRmpi", "parspatstat",
+      "pbdBASE", "pbdDMAT", "pbdMPI", "pbdSLAP",
+      "pmclust", "rpud", "sprint", # mpi
 # Unix-only
-      "R4dfp", "RBerkeley", "ROracleUI", "RProtoBuf",  "TSpadi", "VBmix",
-      "WINRPACK", "cit", "cmprskContin", "doMC", "fork", "gcbd",
-      "interactivity", "makesweave", "mfr", "multic", "multicore", "nice",
-      "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr", "gearman",
-      "vcf2geno", "STARSEQ", "fdasrvf",
+      "HiPLARM", "R4dfp", "RBerkeley", "ROracleUI", "RProtoBuf",
+      "STARSEQ", "TSpadi", "VBmix", "WINRPACK", "cit", "cmprskContin",
+      "doMC", "fork", "fdasrvf", "gcbd" , "gearman", "interactivity",
+      "makesweave", "mfr", "multic", "multicore", "nice",
+      "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr",
+      "vcf2geno",
 # don't work
       "excel.link", # RDCOMClient
       "RWinEdt", # needs Rgui
       "rJavax", # horrible Java things
       ## and for 32-bit-only
-      "hdf5", "satin"
+      "hdf5", "satin",
+      "SNPRelate" # runs forever
       )
 
-biarch <- c("tiff")
+biarch <- c()
 
 multi <- c("BayesXsrc", "C50", "Cairo", "Cubist", "FastRWeb",
            "GWAtoolbox", "JavaGD", "RCurl", "RInside", "RJSONIO",
