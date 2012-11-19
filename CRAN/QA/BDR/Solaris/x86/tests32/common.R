de7308bef322fa8dbebca0bf982bbaa997b35248
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2012-11-19 09:12:35 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2757 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Solaris/x86/tests32/common.R
+++ CRAN/QA/BDR/Solaris/x86/tests32/common.R
@@ -1,19 +1,19 @@
-stoplist <- c("BRugs", "ExomeDepth", "GDD", "aroma.apd",
+stoplist <- c("ExomeDepth", "GDD", "aroma.apd",
               "aroma.cn", "aroma.core", "aroma.affymetrix", "calmate",
-              "ACNE", "MAMA", "NSA",
-              "PKgraph", "beadarrayMSV",
-              "StochaTR", "topologyGSA", "ppiPre", "SNPMaP", "SRMA",
-	"kml", "kml3d", "longitudinalData")
-stoplist <- c(stoplist, "Rcpp") # 0.9-8 segfaults
+              "ACNE", "NSA",
+              "rggobi", "PKgraph", "beadarrayMSV",
+              "StochaTR", "topologyGSA", "ppiPre", "SNPMaP", "SRMA")
+stoplist <- c(stoplist, "Rcpp")
+
 
 fakes <-
-    c("BTSPAS", "CARramps", "GridR", "OpenCL", "RBerkeley", "RDF", "R2OpenBUGS",
+    c("BRugs","BTSPAS", "CARramps", "GridR", "OpenCL", "RBerkeley", "RDF", "R2OpenBUGS",
       "RDieHarder", "RMark", "RMongo", "RMySQL", "ROAuth", "ROracle",
-      "RProtoBuf", "RQuantLib", "RScaLAPACK", "Rcplex", "RiDMC",
+      "RScaLAPACK", "Rcplex", "RiDMC",
       "Rmosek", "SV", "TSMySQL", "VBmix", "WideLM", "cmprskContin",
       "cplexAPI", "cudaBayesreg", "gputools", "magma", "mpc",
       "qtbase", "qtpaint", "rJavax", "rpud", "rpvm", "rscproxy", "rzmq",
-      "RcppOctave")
+      "RcppOctave", "HiPLARM", "RAppArmor")
 
 ll <- c("## Fake installs",
         paste(fakes, "-OPTS = --install=fake", sep=""))
@@ -25,6 +25,6 @@ recommended <-
       "rpart", "spatial", "survival")
 
 gcc <- c("BayesXsrc", "ElectroGraph", "MCMCpack", "MasterBayes",
-          "RGtk2", "Ratings", "glmnet", "gof", "mRm", "phcfM", "rbamtools",
-          "smoothSurv", "sparsenet", "tgp")
+          "RGtk2", "Ratings", "gearman", "glmnet", "gof", "intervals", "mRm", "phcfM", "rbamtools",
+          "smoothSurv", "sparsenet", "tgp", "vcf2geno", "RJSONIO", "mixcat", "PKI", "RSclient")
 
