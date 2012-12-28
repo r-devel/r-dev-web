aac8f344b0cdf513a1e851388d67753e0ca1d701
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2012-12-28 12:05:30 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2773 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Windows/tests/exceptions.R
+++ CRAN/QA/BDR/Windows/tests/exceptions.R
@@ -4,22 +4,23 @@ stoplist <-
       "codetools", "foreign", "lattice",  "mgcv", "nlme", "nnet",
       "rpart", "spatial", "survival",
 # Missing external software
-      "GDD", "OpenCL", "RDF","RDieHarder", "RMongo", "ROAuth", "ROracle",
+      "OpenCL", "RDF","RDieHarder", "RMongo", "ROAuth", "ROracle",
       "Rcplex", "RScaLAPACK", "Rlof", "Rmosek", "cplexAPI",
       "mimR", "mvgraph", "rzmq",
-      "ncdf4", "M3", # ncdf4
-      "CARramps", "WideLM", "cudaBayesreg", "gputools", "magma", # cuda
+      "ncdf4", "M3", "qat", # ncdf4
+      "CARramps", "WideLM", "cudaBayesreg", "gputools", "magma", "permGPU", # cuda
       "qtbase", "qtpaint", "qtutils", "ProgGUIinR",
       "IQMNMR", "Rmpi", "bcool", "doMPI", "npRmpi", "parspatstat",
       "pbdBASE", "pbdDMAT", "pbdMPI", "pbdSLAP",
       "pmclust", "rpud", "sprint", # mpi
+      "RMySQL", "TSMySQL", "dbConnect",
 # Unix-only
       "HiPLARM", "R4dfp", "RBerkeley", "ROracleUI", "RProtoBuf",
       "STARSEQ", "TSpadi", "VBmix", "WINRPACK", "cit", "cmprskContin",
       "doMC", "fork", "fdasrvf", "gcbd" , "gearman", "interactivity",
       "makesweave", "mfr", "multic", "multicore", "nice",
       "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr", "vcf2geno",
-      "bigmemory", "biganalytics", "bigmemory.sri", "bigtabulate", "BerkeleyEarth", "RghcnV3",  "planor",
+      "bigmemory", "biganalytics", "bigmemory.sri", "bigtabulate", "BerkeleyEarth", "PopGenome", "RghcnV3",  "planor",
 # don't work
       "excel.link", # RDCOMClient
       "RWinEdt", # needs Rgui
