c6b17beab1b413b7f2f4c7ab4cf9d93a553e6d7b
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-05-31 08:23:56 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2875 c52295ea-58df-0310-926a-d16021944841

---
+++ CRAN/QA/BDR/blackswan/reinstall.R
@@ -0,0 +1,25 @@
+foo <- row.names(installed.packages(.libPaths()[1]))
+
+chooseBioCmirror(ind = 3)
+setRepositories(ind = 1:6)
+
+Sys.setenv(DISPLAY = ':5',
+           RMPI_TYPE = "OPENMPI",
+           RMPI_INCLUDE = "/usr/include/openmpi-x86_64",
+           RMPI_LIB_PATH = "/usr/lib64/openmpi/lib")
+
+mosek <- path.expand("~/extras/mosek/6")
+Sys.setenv(MOSEKLM_LICENSE_FILE = file.path(mosek, "licenses/mosek.lic"),
+           PKG_MOSEKHOME = file.path(mosek, "tools/platform/linux64x86"),
+           PKG_MOSEKLIB = "mosek64",
+           LD_LIBRARY_PATH = file.path(mosek, "tools/platform/linux64x86/bin"))
+
+if(FALSE)
+Sys.setenv(PATH = paste("/data/blackswan/ripley/extras/bin",
+			Sys.getenv("PATH"), sep = ":"))
+
+opts <- list(Rserve = "--without-server",
+             RNetCDF = "--with-netcdf-include=/usr/include/udunits2",
+             udunits2 = "--with-udunits2-include=/usr/include/udunits2")
+
+install.packages(foo, configure.args = opts, Ncpus = 30)
