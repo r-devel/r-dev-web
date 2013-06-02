4df40cc402b2fe776d32eb18d41638f298da8aad
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-06-02 12:57:40 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2886 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Windows/tests/exceptions.R
+++ CRAN/QA/BDR/Windows/tests/exceptions.R
@@ -5,8 +5,7 @@ stoplist <-
       "rpart", "spatial", "survival",
 # Missing external software
       "OpenCL", "RDieHarder", "REBayes", "RMongo", "ROAuth", "ROracle",
-      "Rcplex", "RSAP", "Rlof", "Rmosek", "cplexAPI",
-      "mimR", "mvgraph", "rzmq",
+      "Rcplex", "RSAP", "Rmosek", "cplexAPI", "mvgraph", "nloptr", "rzmq",
       "ncdf4", "M3", "qat", # ncdf4
       "CARramps", "WideLM", "cudaBayesreg", "gputools", "magma", "permGPU", # cuda
       "qtbase", "qtpaint", "qtutils", "ProgGUIinR",
@@ -15,8 +14,8 @@ stoplist <-
       "pmclust", "rpud", "sprint", # mpi
       "RMySQL", "TSMySQL", "dbConnect",
 # Unix-only
-      "HiPLARM", "R4dfp", "RBerkeley", "ROracleUI", "RProtoBuf", "SGP",
-      "STARSEQ", "VBmix", "WINRPACK", "cit", "doMC", "fork", "fdasrvf",
+      "HiPLARM", "R4dfp", "RBerkeley", "ROracleUI", "RProtoBuf", "RbioRXN",
+      "SGP", "STARSEQ", "VBmix", "WINRPACK", "cit", "doMC", "fork", "fdasrvf",
       "gcbd" , "gearman", "interactivity", "makesweave", "mfr", "multic",
       "multicore", "nice",
       "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr", "vcf2geno",
@@ -31,7 +30,7 @@ stoplist <-
 biarch <- c("PKI", "RSclient")
 
 multi <- c("BayesXsrc", "C50", "Cairo", "Cubist", "FastRWeb",
-           "GWAtoolbox", "JavaGD", "RCurl", "RInside", "RJSONIO",
+           "GWAtoolbox", "JavaGD", "RCA", "RCurl", "RInside", "RJSONIO",
            "RMySQL", "RPostgreSQL", "Rserve", "Rssa", "SWATmodel",
            "bigmemory", "maps", "rJava", "rcom", "rgl", "tth")
 
