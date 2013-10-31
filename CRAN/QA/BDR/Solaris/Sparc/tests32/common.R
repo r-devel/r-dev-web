00187b66f9b3e24683d13c612bf3c65af8964856
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-10-31 07:15:55 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2937 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Solaris/Sparc/tests32/common.R
+++ CRAN/QA/BDR/Solaris/Sparc/tests32/common.R
@@ -2,18 +2,17 @@ stoplist <- c("BRugs", "ExomeDepth",
               "aroma.apd", "aroma.cn", "aroma.core", "aroma.affymetrix", "calmate",
               "ACNE", "MAMA", "NSA",
               "rggobi", "PKgraph", "beadarrayMSV",
-              "StochaTR", "SNPMaP", "SRMA","RMySQL", "TSMySQL", "dbConnect", "Causata")
-
-fakes <-
-    c("BTSPAS", "CARramps", "CARrampsOcl", "GridR", "OpenCL",
-      "RBerkeley", "RDF", "R2OpenBUGS",
+              "StochaTR", "SNPMaP", "SRMA","RMySQL", "TSMySQL", "dbConnect", "Causata",
+      "CARramps", "CARrampsOcl", "GridR", "OpenCL",
+      "RBerkeley", "RDF",
       "RDieHarder", "RMark", "RMongo", "ROAuth", "ROracle",
       "RQuantLib", "RScaLAPACK", "Rcplex", "RiDMC",
-      "Rmosek", "SV", "VBmix", "WideLM", "cmprskContin",
-      "cplexAPI", "cudaBayesreg", "gputools", "magma", "mpc", "permGPU",
-      "qtbase", "qtpaint", "rJavax", "rpud", "rpvm", "rscproxy", "rzmq",
+      "Rmosek", "VBmix", "WideLM", "cmprskContin",
+      "cplexAPI", "cudaBayesreg", "gputools", "gmatrix", "magma", "mpc", "permGPU",
+      "qtbase", "qtpaint", "qtutils", "rJavax", "rpud", "rpvm", "rscproxy", "rzmq", "twitteR",
       "RcppOctave", "HiPLARM", "RAppArmor", "RSAP", "REBayes", "ora", "rLindo")
 
+fakes <- "ROracle"
 
 ll <- c("## Fake installs",
         paste(fakes, "-OPTS = --install=fake", sep=""))
