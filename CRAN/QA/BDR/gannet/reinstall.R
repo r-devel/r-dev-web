3d52a678b092eb85cf5514d384c6a628ca103ef0
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-10-31 07:19:00 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2938 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/reinstall.R
+++ CRAN/QA/BDR/gannet/reinstall.R
@@ -1,9 +1,13 @@
 foo <- row.names(installed.packages(.libPaths()[1]))
 foo <- setdiff(foo, "ROracle")
 
-#chooseBioCmirror(ind = 3)
+if(getRversion() >= "3.1.0") {
+chooseBioCmirror(ind = 3)
+setRepositories(ind = c(1:5,7))
+} else {
 chooseBioCmirror(ind = 5)
 setRepositories(ind = 1:6)
+}
 
 Sys.setenv(DISPLAY = ':5',
            RMPI_TYPE = "OPENMPI",
