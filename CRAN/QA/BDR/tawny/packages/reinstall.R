91ab8740c4cf0fdc13b2213628eefc0f28a2e5fb
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-11-01 11:24:18 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2943 c52295ea-58df-0310-926a-d16021944841

---
+++ CRAN/QA/BDR/tawny/packages/reinstall.R
@@ -0,0 +1,18 @@
+foo <- row.names(installed.packages(.libPaths()[1]))
+
+if(getRversion() >= "3.1.0") {
+chooseBioCmirror(ind = 3)
+setRepositories(ind = c(1:5,7))
+} else {
+chooseBioCmirror(ind = 5)
+setRepositories(ind = 1:6)
+}
+
+Sys.setenv(DISPLAY = ':5', NOAWT = "1", RMPI_TYPE = "OPENMPI",
+          RGL_USE_NULL = "true", PG_INCDIR = "libpq")
+
+## fails if done in parallel
+ex <- c('rJava', 'nloptr','clusterCrit','excursions', 'iplots')
+install.packages(ex, Ncpus = 1)
+foo <- setdiff(foo, c(ex, "ROracle", "Rsymphony", "RandomFields", "intervals", "maxent", "ncdf4", "pedigree", "rgl"))
+install.packages(foo, Ncpus = 10)
