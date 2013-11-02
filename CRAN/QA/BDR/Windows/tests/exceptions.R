4640033751b73963034f5ba4dd42a7adcdc059b0
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-11-02 10:43:07 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2945 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Windows/tests/exceptions.R
+++ CRAN/QA/BDR/Windows/tests/exceptions.R
@@ -4,27 +4,31 @@ stoplist <-
       "codetools", "foreign", "lattice",  "mgcv", "nlme", "nnet",
       "rpart", "spatial", "survival",
 # Missing external software
-      "OpenCL", "RDieHarder", "REBayes", "RMongo", "ROAuth", "ROracle",
-      "Rcplex", "RSAP", "Rmosek", "cplexAPI", "mvgraph", "nloptr", "rzmq",
+      "CARrampsOcl", "OpenCL", "RDieHarder", "REBayes", "RMongo",
+      "ROAuth", "ROracle", "Rcplex", "RSAP", "Rmosek", "cplexAPI", "mvgraph",
+      "nloptr", "ora", "rLindo", "rzmq",
       "ncdf4", "M3", "qat", # ncdf4
       "CARramps", "WideLM", "cudaBayesreg", "gputools", "magma", "permGPU", # cuda
       "qtbase", "qtpaint", "qtutils", "ProgGUIinR",
       "IQMNMR", "Rmpi", "bcool", "doMPI", "npRmpi", "parspatstat",
       "pbdBASE", "pbdDEMO", "pbdDMAT", "pbdMPI", "pbdSLAP",
       "pmclust", "rpud", "sprint", # mpi
-      "RMySQL", "TSMySQL", "dbConnect",
+      "RMySQL", "Causata", "TSMySQL", "dbConnect",
+      "RcppOctave", "rmongodb",
 # Unix-only
       "HiPLARM", "R4dfp", "RBerkeley", "ROracleUI", "RProtoBuf", "RbioRXN",
       "SGP", "STARSEQ", "VBmix", "WINRPACK", "cit", "doMC", "fork", "fdasrvf",
-      "gcbd" , "gearman", "interactivity", "makesweave", "mfr", "multic",
-      "multicore", "nice",
+      "gcbd" , "gearman", "gpmap", "interactivity", "makesweave", "mfr",
+      "multic", "multicore", "nice",
       "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr", "vcf2geno",
-      "bigmemory", "biganalytics", "bigmemory.sri", "bigtabulate", "BerkeleyEarth", "PopGenome", "RghcnV3",  "planor", "bigrf",
+      "bigmemory", "biganalytics", "bigmemory.sri", "bigtabulate",
+      "BerkeleyEarth", "PopGenome", "RghcnV3",  "planor", "bigrf", "Rdsm",
 # don't work
       "excel.link", # RDCOMClient
       "RWinEdt", # needs Rgui
       "rJavax", # horrible Java things
-      "SNPRelate" # runs forever
+      "SNPRelate", # runs forever
+      "Modalclust", "StreamingLm", "drmdel", "parallelize.dynamic", "translate"
       )
 
 biarch <- c("PKI", "RSclient")
@@ -32,6 +36,6 @@ biarch <- c("PKI", "RSclient")
 multi <- c("BayesXsrc", "C50", "Cairo", "Cubist", "FastRWeb",
            "GWAtoolbox", "JavaGD", "RCA", "RCurl", "RInside", "RJSONIO",
            "RMySQL", "RPostgreSQL", "Rserve", "Rssa", "SWATmodel",
-           "bigmemory", "maps", "rJava", "rcom", "rgl", "tth")
+           "excursions", "maps", "rJava", "rcom", "rgl", "tth")
 
 extras <- c("XMLRPC", "yags")
