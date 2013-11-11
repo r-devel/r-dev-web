5df0a04859617115fd4e8370c5cd78ff248e9440
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-11-11 15:59:32 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2951 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/tawny/packages/reinstall.R
+++ CRAN/QA/BDR/tawny/packages/reinstall.R
@@ -13,5 +13,5 @@ Sys.setenv(DISPLAY = ':5', NOAWT = "1", RMPI_TYPE = "OPENMPI",
 ## fails if done in parallel
 ex <- c('rJava', 'nloptr','clusterCrit','excursions', 'iplots')
 install.packages(ex, Ncpus = 1)
-foo <- setdiff(foo, c(ex, "ROracle", "Rsymphony", "RandomFields", "bigtabulate","maxent", "ncdf4"))
+foo <- setdiff(foo, c(ex, "ROracle", "Rsymphony", "maxent", "ncdf4"))
 install.packages(foo, Ncpus = 10)
--- CRAN/QA/BDR/tawny/packages/updates.R
+++ CRAN/QA/BDR/tawny/packages/updates.R
@@ -1,7 +1,7 @@
 source("common.R")
 
 stoplist <- c(stoplist, "npRmpi",
-"ConConPiWiFun", "Rsymphony", "bigtabulate", "cda", "cheddar", "gearman", "maxent", "planar", "synchronicity", "tmg")
+"Rsymphony", "cheddar", "gearman", "maxent",  "tmg")
 
 Sys.setenv(DISPLAY = ':5', NOAWT = "1", RMPI_TYPE = "OPENMPI",
           RGL_USE_NULL = "true", PG_INCDIR = "libpq")
