f0a7fa1b05dfde5f4d9e5bd67dc4c3bc00f463d5
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2013-12-25 08:10:09 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2975 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/tests-clang2/process.R
+++ CRAN/QA/BDR/gannet/tests-clang2/process.R
@@ -3,10 +3,13 @@ files <- Sys.glob("*.Rcheck/*.Rout")
 for(f in files) {
     l <- readLines(f)
     ll <- grep('runtime error', l, value = TRUE, useBytes = TRUE)
-    ll <- grep('division by zero', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
+#    ll <- grep('division by zero', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
+    ll <- grep('/R-devel/src', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
     if(length(ll)) {
+	cat(".")
         file.copy(f, "/data/ftp/pub/bdr/memtests/UBSAN", overwrite=TRUE)
         Sys.setFileTime(file.path("/data/ftp/pub/bdr/memtests/UBSAN",
                                   basename(f)), file.info(f)$mtime)
     }
 }
+cat("\n")
