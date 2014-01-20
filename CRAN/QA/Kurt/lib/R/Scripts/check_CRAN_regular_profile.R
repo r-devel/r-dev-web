80ff51c5cfa3ba26540b25e49c19d8dc6a8898b9
hornik <hornik@c52295ea-58df-0310-926a-d16021944841>
2014-01-20 09:03:06 +0000

    Added.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2983 c52295ea-58df-0310-926a-d16021944841

---
+++ CRAN/QA/Kurt/lib/R/Scripts/check_CRAN_regular_profile.R
@@ -0,0 +1,20 @@
+if(getRversion() >= "2.7.0") {
+    setHook(packageEvent("grDevices", "onLoad"),
+            function(...) {
+                grDevices::X11.options(type = "Cairo")
+            })
+} else {
+    options(X11colortype = "pseudo.cube")
+}
+
+## See check_repository_URLs() in check_CRAN_regular.R.
+local({
+    if(nzchar(s <- Sys.getenv("_CHECK_CRAN_REGULAR_REPOSITORIES_"))) {
+        s <- unlist(strsplit(s, "[=;]"))
+        ind <- as.logical(seq_along(s) %% 2L)
+        repos <- s[!ind]
+        names(repos) <- s[ind]
+
+        options(repos = repos)
+    }
+})
