2944b23cbd2344f7c72ed7756734b66517684ad1
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-02-18 08:10:12 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2997 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/tests-devel/unpack.sh
+++ CRAN/QA/BDR/blackswan/tests-devel/unpack.sh
@@ -7,7 +7,7 @@ if test $p -nt $pkgname.in; then
   touch -r $p $pkgname.in
 fi
 done
-for p in ../3.1.0/Recommended/MASS*.tar.gz; do
+for p in ../3.1.0/Other/*.tar.gz; do
   pkgname=`basename $p | sed  -e 's/_.*//'`
 if test $p -nt $pkgname.in; then
   echo $pkgname
