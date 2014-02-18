94beec261384d8fb2b78d977a72946be5563fff1
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-02-18 08:17:01 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@2998 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/tests-clang2/process.R
+++ CRAN/QA/BDR/gannet/tests-clang2/process.R
@@ -7,9 +7,44 @@ for(f in files) {
     ll <- grep('/R-devel/src', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
     if(length(ll)) {
 	cat(".")
-        file.copy(f, "/data/ftp/pub/bdr/memtests/UBSAN", overwrite=TRUE)
+        file.copy(f, "/data/ftp/pub/bdr/memtests/UBSAN", overwrite=TRUE, copy.date = TRUE)
         Sys.setFileTime(file.path("/data/ftp/pub/bdr/memtests/UBSAN",
                                   basename(f)), file.info(f)$mtime)
     }
 }
 cat("\n")
+
+files <- Sys.glob("*.Rcheck/tests/*.Rout")
+for(f in files) {
+    l <- readLines(f)
+    ll <- grep('runtime error', l, value = TRUE, useBytes = TRUE)
+    ll <- grep('/R-devel/src', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
+    if(length(ll)) {
+	cat(".")
+        ff <- sub("[.]Rcheck/.*", "", f)
+        dir.create(file.path("/data/ftp/pub/bdr/memtests/UBSAN", ff, "tests"),
+                             showWarnings = FALSE, recursive = TRUE)
+        f2 <- sub(".*[.]Rcheck/", "", f)
+        file.copy(f,file.path("/data/ftp/pub/bdr/memtests/UBSAN", ff, f2), overwrite=TRUE, copy.date = TRUE)
+    }
+}
+cat("\n")
+
+files <- c(Sys.glob("*.Rcheck/*.[RSrs]nw.log"),
+           Sys.glob("*.Rcheck/*.[RSrs]tex.log"))
+for(f in files) {
+    l <- readLines(f)
+    ll <- grep('runtime error', l, value = TRUE, useBytes = TRUE)
+    ll <- grep('/R-devel/src', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
+    if(length(ll)) {
+        cat(".")
+        ff <- sub("[.]Rcheck/.*", "", f)
+        dir.create(file.path("/data/ftp/pub/bdr/memtests/UBSAN", ff),
+                             showWarnings = FALSE, recursive = TRUE)
+        f2 <- sub(".*[.]Rcheck/", "", f)
+        file.copy(f,file.path("/data/ftp/pub/bdr/memtests/UBSAN", ff, f2), overwrite=TRUE, copy.date = TRUE)
+    }
+}
+cat("\n")
+
+
