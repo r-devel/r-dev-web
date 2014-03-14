5f8ebaad99360c8b78fc3504ce426d66c2813b4c
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-03-14 10:53:50 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3012 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/updates.R
+++ CRAN/QA/BDR/blackswan/updates.R
@@ -1,6 +1,7 @@
 stoplist <-
 c("CARramps", "HiPLARM", "DeducerMMR", "RAppArmor", "RDF", "RDieHarder", "RMark", "RMongo", "ROracle", "ROracleUI", "RQuantLib", "RSAP", "RScaLAPACK", "Rcplex", "Rhpc", "Rmosek", "WideLM", "cplexAPI", "cudaBayesreg", "gputools", "magma", "ora", "permGPU", "rJavax", "gmatrix", "rpud", "rscproxy", "Rpoppler", "rLindo")
 
+stoplist <- c(stoplist, "RcppOctave", "OpenCL", "CARrampsOcl")
 
 mosek <- path.expand("~/extras/mosek/6")
 Sys.setenv(MOSEKLM_LICENSE_FILE = file.path(mosek, "licenses/mosek.lic"),
