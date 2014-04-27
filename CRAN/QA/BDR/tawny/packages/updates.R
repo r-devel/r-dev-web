53e2a036310de254185aeba786d811ab6902e12e
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-04-27 07:24:13 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3060 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/tawny/packages/updates.R
+++ CRAN/QA/BDR/tawny/packages/updates.R
@@ -1,19 +1,18 @@
+options(available_packages_filter =
+     c("R_version", "OS_type", "subarch", "CRAN", "duplicates"))
+
 source("common.R")
 
 Sys.setenv(DISPLAY = ':5', NOAWT = "1", RMPI_TYPE = "OPENMPI",
           RGL_USE_NULL = "true", PG_INCDIR = "libpq")
 
 
-if(getRversion() >= "3.1.0") {
 setRepositories(ind = c(1:5,7))
-} else {
-setRepositories(ind = 1:6)
-}
 update.packages(ask=FALSE)
 setRepositories(ind=1)
 new <- new.packages()
 new <- new[! new %in% stoplist]
 if(length(new)) {
-    setRepositories(ind = 1:5)
+    setRepositories(ind = c(1:5,7))
     install.packages(new)
 }
