3eb68b9752e30a565c11591487d2b020a25e1dc4
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-04-30 06:40:04 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3072 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Solaris/x86/packages/reinstall.R
+++ CRAN/QA/BDR/Solaris/x86/packages/reinstall.R
@@ -2,10 +2,10 @@
 foo <- row.names(installed.packages(.libPaths()[1]))
 
 chooseBioCmirror(ind = 5)
-setRepositories(ind = c(2:5, ifelse(getRversion() >= "3.1.0", 7,6)))
+setRepositories(ind = c(2:5, 7))
 foo2 <- row.names(available.packages())
 foo <- intersect(foo, foo2)
-foo <- setdiff(foo, c("RCurl", "XML")) # Omegahat duplicates
+foo <- setdiff(foo, c("RCurl", "RJSONIO", "XML")) # Omegahat duplicates
 
 Sys.setenv(DISPLAY = ':5', MAKE = 'gmake')
 
