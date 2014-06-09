2b484543cc7e2e44d2c2accc44c5baa4d912f7e2
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-06-09 07:19:21 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3093 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/reinstall.R
+++ CRAN/QA/BDR/blackswan/reinstall.R
@@ -1,4 +1,4 @@
-options(available_packages_filter =
+options(available_packages_filters =
      c("R_version", "OS_type", "subarch", "CRAN", "duplicates"))
 
 foo <- row.names(installed.packages(.libPaths()[1]))
@@ -6,7 +6,8 @@ foo <- row.names(installed.packages(.libPaths()[1]))
 foo <- setdiff(foo, "ROracle")
 
 if(getRversion() >= "3.2.0") chooseBioCmirror(ind = 3) else chooseBioCmirror(ind = 5)
-setRepositories(ind = c(1:5,7))
+#setRepositories(ind = c(1:5,7))
+setRepositories(ind = c(1:4,7))
 
 Sys.setenv(DISPLAY = ':5',
            RMPI_TYPE = "OPENMPI",
@@ -27,5 +28,6 @@ opts <- list(Rserve = "--without-server",
              RNetCDF = "--with-netcdf-include=/usr/include/udunits2",
              udunits2 = "--with-udunits2-include=/usr/include/udunits2")
 
-install.packages("ROracle", INSTALL_opts = "--fake")
-install.packages(foo, configure.args = opts, Ncpus = 30)
+opts2 <- list(ROracle = "--fake")
+
+install.packages(foo, configure.args = opts, INSTALL_opts = opts2, Ncpus = 30)
