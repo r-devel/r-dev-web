93b3ddb4750ce636e3747c13eac67752c906783a
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-08-14 06:38:17 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3143 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/tests-vg/unpack.R
+++ CRAN/QA/BDR/blackswan/tests-vg/unpack.R
@@ -1,4 +1,4 @@
-stoplist <- c("RcppOctave", "OpenCL", "CARrampsOcl")
+stoplist <- c("RcppOctave", "OpenCL", "CARrampsOcl", "sanitizers")
 
 list_tars <- function(dir='.')
 {
@@ -9,7 +9,7 @@ list_tars <- function(dir='.')
 }
 
 foo1 <- list_tars('../contrib')
-foo <- list_tars('../3.1.0/Other')
+foo <- list_tars('../3.2.0/Other')
 foo <- rbind(foo, foo1)
 tars <- foo[!duplicated(foo$name), ]
 nm <- tars$name
