3636aee215eea00ec469495d223b81ecaa7e74af
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-09-24 06:58:13 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3170 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/tests-50/process.R
+++ CRAN/QA/BDR/gannet/packages/tests-50/process.R
@@ -1,31 +1,31 @@
-files <- Sys.glob("*.Rcheck/00check.log")
+files <- Sys.glob("*.Rcheck/*.Rout")
+
 for(f in files) {
     l <- readLines(f)
-    ll <- grep('(ASan internal:|SUMMARY: AddressSanitizer: alloc-dealloc-mismatch)', l, value = TRUE, useBytes = TRUE)
+    ll <- grep('runtime error', l, value = TRUE, useBytes = TRUE)
+#    ll <- grep('division by zero', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
+    ll <- grep('(/R-devel/src|downcast of address)', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
     if(length(ll)) {
-        cat(".")
-        ff <- sub("[.]Rcheck/.*", "", f)
-	f2 <- dirname(f)
-        dir.create(file.path("/data/ftp/pub/bdr/memtests/ASAN", ff),
-                             showWarnings = FALSE, recursive = TRUE)
-        file.copy(f, file.path("/data/ftp/pub/bdr/memtests/ASAN", ff, "00check.log"), 
-                  overwrite=TRUE, copy.date = TRUE)
+	cat(".")
+        file.copy(f, "/data/ftp/pub/bdr/memtests/UBSAN-gcc", overwrite=TRUE, copy.date = TRUE)
+        Sys.setFileTime(file.path("/data/ftp/pub/bdr/memtests/UBSAN-gcc",
+                                  basename(f)), file.info(f)$mtime)
     }
 }
 cat("\n")
 
-
-files <- Sys.glob("*.Rcheck/tests/*.Rout.fail")
+files <- Sys.glob("*.Rcheck/tests/*.Rout")
 for(f in files) {
     l <- readLines(f)
-    ll <- grep('ASan internal:', l, value = TRUE, useBytes = TRUE)
+    ll <- grep('runtime error', l, value = TRUE, useBytes = TRUE)
+    ll <- grep('(/R-devel/src|downcast of address)', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
     if(length(ll)) {
 	cat(".")
         ff <- sub("[.]Rcheck/.*", "", f)
-        dir.create(file.path("/data/ftp/pub/bdr/memtests/ASAN", ff, "tests"),
+        dir.create(file.path("/data/ftp/pub/bdr/memtests/UBSAN-gcc", ff, "tests"),
                              showWarnings = FALSE, recursive = TRUE)
         f2 <- sub(".*[.]Rcheck/", "", f)
-        file.copy(f,file.path("/data/ftp/pub/bdr/memtests/ASAN", ff, f2), overwrite=TRUE, copy.date = TRUE)
+        file.copy(f,file.path("/data/ftp/pub/bdr/memtests/UBSAN-gcc", ff, f2), overwrite=TRUE, copy.date = TRUE)
     }
 }
 cat("\n")
@@ -34,20 +34,21 @@ files <- c(Sys.glob("*.Rcheck/*.[RSrs]nw.log"),
            Sys.glob("*.Rcheck/*.[RSrs]tex.log"))
 for(f in files) {
     l <- readLines(f)
-    ll <- grep('ASan internal:', l, value = TRUE, useBytes = TRUE)
+    ll <- grep('runtime error', l, value = TRUE, useBytes = TRUE)
+    ll <- grep('(/R-devel/src|downcast of address)', ll, invert = TRUE, value = TRUE, useBytes = TRUE)
     if(length(ll)) {
         cat(".")
         ff <- sub("[.]Rcheck/.*", "", f)
-        dir.create(file.path("/data/ftp/pub/bdr/memtests/ASAN", ff),
+        dir.create(file.path("/data/ftp/pub/bdr/memtests/UBSAN-gcc", ff),
                              showWarnings = FALSE, recursive = TRUE)
         f2 <- sub(".*[.]Rcheck/", "", f)
-        file.copy(f,file.path("/data/ftp/pub/bdr/memtests/ASAN", ff, f2), overwrite=TRUE, copy.date = TRUE)
+        file.copy(f,file.path("/data/ftp/pub/bdr/memtests/UBSAN-gcc", ff, f2), overwrite=TRUE, copy.date = TRUE)
     }
 }
 cat("\n")
 
-for(d in dir('/data/ftp/pub/bdr/memtests/ASAN')) {
-    Sys.setFileTime(file.path("/data/ftp/pub/bdr/memtests/ASAN", d),
-                    file.info(paste0(d, ".Rcheck"))$mtime)
+for(d in list.dirs('/data/ftp/pub/bdr/memtests/UBSAN-gcc', TRUE, FALSE)) {
+    Sys.setFileTime(d, file.info(paste0(basename(d), ".Rcheck"))$mtime)
 }
+
 
