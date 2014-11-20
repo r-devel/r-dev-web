ebb72c12658e02e4c7736e21cff490b59b105b89
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-11-20 08:33:32 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3205 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/auk/packages/reinstall.R
+++ CRAN/QA/BDR/auk/packages/reinstall.R
@@ -3,7 +3,7 @@ options(available_packages_filters =
 
 foo <- row.names(installed.packages(.libPaths()[1]))
 
-#chooseBioCmirror(ind = 3)
+chooseBioCmirror(ind = 3)
 setRepositories(ind = c(1:4,7))
 
 Sys.setenv(DISPLAY = ':5',
