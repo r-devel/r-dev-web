316574a8c24a2c412c1e2e77b09bc8b75100b6a1
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2014-12-24 07:41:09 +0000

    update
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3220 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Windows/tests/exceptions.R
+++ CRAN/QA/BDR/Windows/tests/exceptions.R
@@ -4,31 +4,29 @@ stoplist <-
       "codetools", "foreign", "lattice",  "mgcv", "nlme", "nnet",
       "rpart", "spatial", "survival",
 # Missing external software
-      "CARrampsOcl", "OpenCL", "RcppOctave", "RDieHarder", "REBayes", "RMongo",
-      "ROAuth", "ROracle", "RSAP", "Rcplex", "RcppRedis", "Rmosek", "cplexAPI",
-      "localsolver", "mvgraph", "ora", "rLindo", "rmongodb", "rzmq",
+      "CARrampsOcl", "OpenCL", "RcppOctave", "RDieHarder", "RMongo",
+      "ROAuth", "ROracle", "RSAP", "Rcplex", "RcppRedis", "cplexAPI",
+      "localsolver", "ora", "rLindo", "rmongodb", "rzmq",
       "ncdf4", "M3", "ncdf4.helpers", "ocean", "qat", # ncdf4
-      "CARramps", "WideLM", "cudaBayesreg", "gmatrix", "gputools", "magma", "permGPU", "rpud", # cuda
+      "CARramps", "HiPLARM", "WideLM", "cudaBayesreg", "gmatrix", "gputools",
+      "iFes", "magma", "permGPU", "rpud", # cuda
       "qtbase", "qtpaint", "qtutils", "ProgGUIinR",
       "bcool", "simsalapar", # mpi
+      "Rmosek", "REBayes", "cqrReg",
       "pbdBASE", "pbdDEMO", "pbdDMAT",
       "RMySQL", "Causata", "TSMySQL", "compendiumdb", "dbConnect",
 # Unix-only
-      "HiPLARM", "OmicKriging", "PAGWAS", "PopGenome", "R4dfp",
-      "RBerkeley", "ROracleUI", "RProtoBuf", "Rdsm", "SGP",
-      "STARSEQ", "VBmix", "WINRPACK", "cit", "fork",
-      "fdasrvf", "gcbd" , "gearman", "gpmap", "interactivity",
-      "makesweave", "mfr", "multic", "multicore", "nice",
-      "polyphemus", "rcqp", "synchronicity", "taskPR", "triggr", "vcf2geno",
+      "OmicKriging", "PAGWAS", "PopGenome", "R4dfp", "RProtoBuf", "Rdsm",
+      "SGP", "VBmix", "cit", "doMC", "fdasrvf", "gcbd" , "gemmR", "gpmap",
+      "multic", "nice", "sprint", "synchronicity",
       "bigmemory", "bigalgebra", "biganalytics", "bigmemory.sri", "bigrf", "bigtabulate",
-      "doMC", "dnet",
 # don't work
-      "JAGUAR", # doMC
+      "JAGUAR", "dcGOR", # doMC
       "excel.link", # RDCOMClient
       "RWinEdt", # needs Rgui
       "rJavax", # horrible Java things
-      "SNPRelate", "Storm", # runs forever
-      "StreamingLm", "parallelize.dynamic", "translate"
+      "Storm", # runs forever
+      "parallelize.dynamic", "translate", "RCMIP5"
       )
 
 biarch <- c("PKI", "RSclient", "R2SWF")
@@ -36,6 +34,6 @@ biarch <- c("PKI", "RSclient", "R2SWF")
 multi <- c("BayesXsrc", "C50", "Cairo", "Cubist", "FastRWeb", "GWAtoolbox",
            "JavaGD", "RCA", "RCurl", "RInside", "RJSONIO", "RMySQL",
            "RPostgreSQL", "Rserve", "Rssa", "SWATmodel", "dbarts",
-           "excursions", "jsonlite", "maps", "rJava", "rgl", "tth")
+           "excursions", "jsonlite", "maps", "ore", "rJava", "rgl", "tth")
 
 extras <- c("XMLRPC", "yags")
