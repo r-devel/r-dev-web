bcd42ad57d297ac9b671b406fab1f229667d6f67
ligges <ligges@c52295ea-58df-0310-926a-d16021944841>
2015-03-20 14:43:09 +0000

    update BioC releases
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3288 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/Uwe/make/BioC-update.R
+++ CRAN/QA/Uwe/make/BioC-update.R
@@ -1,7 +1,8 @@
 Rver <- paste(strsplit(as.character(getRversion()), "\\.")[[1]][1:2], collapse=".")
 
 BioCver <- switch(Rver,
-    "3.2" = "3.0",
+    "3.3" = "3.1",    
+    "3.2" = "3.1",
     "3.1" = "3.0",
     "3.0" = "2.13"
 )
