474ffa49527697509eda7a29dcae4d01dc00a47e
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2015-05-29 07:46:20 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3333 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/common.R
+++ CRAN/QA/BDR/blackswan/common.R
@@ -1,7 +1,8 @@
 stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd',
               'RPyGeo', 'RWinEdt', 'blatr', 'excel.link', 'installr',
               'BayesXsrc', 'R2BayesX', 'rzmq', 'iFes', 'RcppAPT')
-stoplist <- c(stoplist, "RcppOctave", "OpenCL", "CARrampsOcl", "rLindo", "RQuantLib")
+stoplist <- c(stoplist, "RcppOctave", "OpenCL", "CARrampsOcl", "rLindo", "RQuantLib", "TSMySQL")
+stoplist <- c(stoplist, "REBayes", "Rmosek")
 
 
 CUDA <-
