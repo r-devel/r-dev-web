eb4e1596c6851c5fd5f64db1fd6d72d4730c8322
ligges <ligges@c52295ea-58df-0310-926a-d16021944841>
2015-06-18 13:40:56 +0000

    R-3.2.1 updates
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3350 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/Uwe/make/set_recent_Env.bat
+++ CRAN/QA/Uwe/make/set_recent_Env.bat
@@ -1,3 +1,3 @@
-set Path=%PATH%;d:\Rcompile\recent\R-3.2.0\bin;D:\RCompile\recent\R-3.2.0\bin\i386
+set Path=%PATH%;d:\Rcompile\recent\R-3.2.1\bin;D:\RCompile\recent\R-3.2.1\bin\i386
 set maj.version=3.2
 set R_LIBS=d:/Rcompile/CRANpkg/lib/%maj.version%
