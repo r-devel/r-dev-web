3d59dd313cd1f1928078612a8289ec3dc0af86bb
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2015-06-18 06:21:23 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3342 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -1,9 +1,10 @@
 stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd',
               'RPyGeo', 'RWinEdt', 'blatr', 'excel.link', 'installr',
-              'RQuantLib', 'BayesXsrc', 'R2BayesX', 'RcppAPT')
+              'RQuantLib', 'RcppAPT')
 
 CUDA <-
 c("CARramps", "HiPLARM", "RAppArmor", "RDieHarder", "ROracle", "RSAP", "Rcplex", "Rhpc", "WideLM", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "rJavax", "rpud", "localsolver", "iFes")
 
 noclang <- c("RProtoBuf", "RQuantLib", "RVowpalWabbit", "RcppOctave", "h5")
+noclang <- c(noclang, 'qtbase', 'qtpaint', 'qtutils')
 
