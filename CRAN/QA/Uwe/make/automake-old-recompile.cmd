d3bb5b4e2c51ecf20d58e9edf4f7684a868edaf2
ligges <ligges@c52295ea-58df-0310-926a-d16021944841>
2015-06-18 13:41:29 +0000

    need 64-bit R for the build jobs (more than 3GB memory required for the control process)
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3351 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/Uwe/make/automake-old-recompile.cmd
+++ CRAN/QA/Uwe/make/automake-old-recompile.cmd
@@ -1,6 +1,6 @@
 pause
 call d:\RCompile\CRANpkg\make\set_Env_new.bat 
-call d:\RCompile\CRANpkg\make\set_oldrelease_Env.bat 
+call d:\RCompile\CRANpkg\make\set_oldrelease64_Env.bat 
 set mailMaintainer=no
 pause
 
--- CRAN/QA/Uwe/make/automake-old.cmd
+++ CRAN/QA/Uwe/make/automake-old.cmd
@@ -1,5 +1,5 @@
 call d:\RCompile\CRANpkg\make\set_Env_new.bat 
-call d:\RCompile\CRANpkg\make\set_oldrelease_Env.bat 
+call d:\RCompile\CRANpkg\make\set_oldrelease64_Env.bat 
 set mailMaintainer=no
 
 d:
