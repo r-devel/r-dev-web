729f02c3a5796c7d1ffb3cbbb263ed6324fd30e5
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2015-10-16 10:24:53 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3402 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/common.R
+++ CRAN/QA/BDR/blackswan/common.R
@@ -1,12 +1,12 @@
 stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RPyGeo',
               'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
-              'BayesXsrc', 'R2BayesX', 'rzmq', 'iFes', 'RcppAPT')
-stoplist <- c(stoplist, "RcppOctave", "OpenCL", "CARrampsOcl", "rLindo", "RQuantLib", "TSMySQL")
+              'rzmq', 'RcppAPT', 'caRpools')
+stoplist <- c(stoplist, "RcppOctave", "OpenCL", "CARrampsOcl", "rLindo", "RQuantLib", "TSMySQL", "sodium")
 stoplist <- c(stoplist, "REBayes", "Rmosek")
 
 
 CUDA <-
-c("CARramps", "HiPLARM", "RAppArmor", "RDieHarder", "ROracle", "RSAP", "Rcplex", "Rhpc", "WideLM", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "rJavax", "rpud", "localsolver")
+c("CARramps", "HiPLARM", "RAppArmor", "RDieHarder", "ROracle", "RSAP", "Rcplex", "Rhpc", "WideLM", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "rJavax", "rpud", "localsolver", "iFes")
 
 noclang <- c("RProtoBuf", "RQuantLib", "RVowpalWabbit", "RcppOctave")
 
