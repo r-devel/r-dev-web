88d1f430288af0a30ca11016cd1235dc7522a57d
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2015-11-22 08:03:30 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3421 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/Solaris/x86/packages/tests32/common.R
+++ CRAN/QA/BDR/Solaris/x86/packages/tests32/common.R
@@ -1,19 +1,20 @@
 stoplist <- c("rggobi", "PKgraph", "beadarrayMSV", "clusterfly", "SeqGrapheR",
       "Rcell", "RockFab", "gitter", "metagear", # EBImage
-      "MSeasy", "MSeasyTkGUI",
-      "RMySQL", "TSMySQL", "dbConnect", "Causata",  "compendiumdb",
+      "MSeasy", "MSeasyTkGUI", "specmine",
+      "RMySQL", "TSMySQL", "dbConnect", "Causata", "compendiumdb", "wordbankr",
       "BRugs","CARramps", "CARrampsOcl", "GridR", "OpenCL",
       "RBerkeley", "RDieHarder", "RMark", "RMongo", "ROracle",
       "RProtoBuf", "RQuantLib", "RVowpalWabbit", "RcppRedis", "Rcplex", "Rhpc",
       "Rmosek", "VBmix", "WideLM", "cmprskContin",
       "cplexAPI", "cudaBayesreg", "gputools", "gmatrix", "magma", "permGPU",
       "qtbase", "qtpaint", "qtutils", "rJavax", "rmongodb",
-      "rpud", "rpvm", "rscproxy", "rzmq", "twitteR",
+      "rpud", "rpvm", "rscproxy", "rzmq",
       "Rpoppler", "Rsymphony", "ROI.plugin.symphony", "fPortfolio", "BLCOP",
       "RcppOctave", "HiPLARM", "RAppArmor", "RSAP", "REBayes", "ora",
       "rLindo", "localsolver", "Boom", "BoomSpikeSlab", "bsts", "iptools",
       "iFes", "rSPACE", "RcppAPT", "nFCA", "multimark", "h5", "caRpools",
-      "Rblpapi", "PythonInR", "microbenchmark", "timeit", "sodium", "maGUI")
+      "Rblpapi", "PythonInR", "microbenchmark", "timeit", "sodium", "maGUI",
+      "flowDiv", "Goslate", "BEDMatrix", "homomorpheR", "gpuR")
 
 
 WindowsOnly <- c("BiplotGUI", "MDSGUI", "R2MLwiN", "R2PPT", "R2wd", "RPyGeo", "RWinEdt", "TinnR", "blatr", "excel.link", "installr", "spectrino")
@@ -50,7 +51,7 @@ gcc <- c(gcc, "deSolve")
 gcc <- c(gcc, 'RcppParallel', 'StMoSim', 'markovchain', 'rPref')
 
 gcc <- c(gcc, "Rcpp", "RcppArmadillo", "RcppEigen")
-gcc <- c(gcc, "RMessenger", "Rmixmod", "dplyr", "gdsfmt", "httpuv", "mirt", "phylobase", "scrypt", "repfdr", "RJSONIO", "SKAT", "HDPenReg", "FunChisq", "mapfit", "rgdal", "V8", "readxl", "icenReg", "stream", "FCNN4R", "TMB")
+gcc <- c(gcc, "RMessenger", "Rmixmod", "dplyr", "gdsfmt", "httpuv", "mirt", "phylobase", "scrypt", "repfdr", "RJSONIO", "SKAT", "HDPenReg", "FunChisq", "mapfit", "rgdal", "V8", "readxl", "icenReg", "stream", "FCNN4R", "TMB", "funcy", "brms")
 
 Sys.setenv("OPENSSL_INCLUDES" = "/opt/csw/include", CURL_INCLUDES = "/opt/csw/include", "V8_INCLUDES" = "/opt/csw/include")
 
