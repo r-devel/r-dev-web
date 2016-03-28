a26eca9802a5888414882c46e8bb21c80016ed6b
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2016-03-28 08:45:23 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3480 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/tawny/packages/common.R
+++ CRAN/QA/BDR/tawny/packages/common.R
@@ -1,19 +1,19 @@
 stoplist <-
     c("BRugs", "RAppArmor", "RDieHarder", "RMark", "ROracle", "RQuantLib",
       "RSAP", "Rcplex", "RcppOctave", "Rpoppler", "ora", "pdftools", "rLindo",
-      # "Rmosek", "REBayes",
       ## CUDA
       "CARramps", "HiPLARM", "WideLM", "cplexAPI", "cudaBayesreg", "gmatrix",
       "gputools",  'iFes', "localsolver", "magma", "permGPU", "rpud",
       ## qt users
       "qtbase", "qtpaint", "qtutils", "VBmix",
       #"RMySQL", "Causata", "TSMySQL",  "compendiumdb", "dbConnect",
-      "RcppAPT", "caRpools", "rcrypt", "rsvg", "redland", "hunspell",
+      "RcppAPT", "caRpools", "rcrypt", "rsvg", "redland", "hunspell", "datapack",
+      "Boom", "BoomSpikeSlab", "bsts",
       # ggobi no longer builds, binary is linked to old GTK+
       "rggobi", "PKgraph", "SeqGrapheR", "beadarrayMSV", "clusterfly",
       ## Windows-only
       'BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd',
       'RPyGeo', 'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino')
 
-noinstall <- c("R2STATS", "gpuR", 'dynaTree')
+noinstall <- c("R2STATS", "gpuR", 'dynaTree', 'rmumps')
 
