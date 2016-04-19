f07292ff741906300714d72e39c3c0e09636c765
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2016-04-19 09:03:05 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3486 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/tests-50/unpack.R
+++ CRAN/QA/BDR/gannet/packages/tests-50/unpack.R
@@ -12,8 +12,8 @@ list_tars <- function(dir='.')
 }
 
 foo1 <- list_tars('../contrib')
-foo0 <- list_tars('../contrib/3.3.0/Others')
-foo <- list_tars('../contrib/3.3.0/Recommended')
+foo0 <- list_tars('../contrib/3.4.0/Others')
+foo <- list_tars('../contrib/3.4.0/Recommended')
 foo <- rbind(foo, foo0, foo1)
 tars <- foo[!duplicated(foo$name), ]
 nm <- tars$name
