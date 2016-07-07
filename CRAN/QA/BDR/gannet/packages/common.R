20c01a9be06b0f9157a358593b56815e0613d43d
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2016-07-07 08:52:46 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3513 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -1,12 +1,13 @@
 stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RPyGeo',
                'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
               'RQuantLib', 'RcppAPT', 'caRpools',
+		"ROI.plugin.cplex",
 	      "Boom", "BoomSpikeSlab", "bsts")
 
 CUDA <- # etc
-c("HiPLARM", "RAppArmor", "RDieHarder", "ROracle", "RSAP", "Rcplex", "Rhpc", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "localsolver")
+c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "Rcplex", "Rhpc", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "localsolver")
 
 noclang <- c("RQuantLib", "RVowpalWabbit", "RcppOctave", "h5", "pdftools")
 
-noinstall <- c('qtbase', 'qtpaint', 'qtutils', "littler", "gpuR", 'Statomica', 'rcppbugs', 'dfcomb', 'dfmta')
+noinstall <- c('qtbase', 'qtpaint', 'qtutils', "littler", "gpuR", 'Statomica', 'rcppbugs', 'IRTpp', 'RSQLServer', 'aemo')
 
