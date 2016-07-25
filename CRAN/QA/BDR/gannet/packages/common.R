5d3ac979dbbe921ebae8bcd6bbaf60a9e7ed8ca9
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2016-07-25 09:51:03 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3521 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -1,12 +1,16 @@
 stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RPyGeo',
                'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
               'RQuantLib', 'RcppAPT', 'caRpools', "ROI.plugin.cplex")
-#	      "Boom", "BoomSpikeSlab", "bsts")
 
 CUDA <- # etc
 c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "Rcplex", "Rhpc", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "localsolver")
 
-noclang <- c("RQuantLib", "RcppOctave", "h5", "pdftools")
+noclang <- c("RQuantLib", "RcppOctave", "h5", "magick", "pdftools")
 
-noinstall <- c('qtbase', 'qtpaint', 'qtutils', "littler", "gpuR", 'Statomica', 'IRTpp', 'RSQLServer', 'aemo', "Boom", "BoomSpikeSlab", "bsts")
+noinstall <- c("littler", "gpuR", 'Statomica', 'RSQLServer', 'aemo', "mvst",
+   'deeplearning', 'sbrl', 'seqHMM')
+
+noinstall_clang <- 
+c('qtbase', 'qtpaint', 'qtutils', "Boom", "BoomSpikeSlab", "bsts",
+  'IntegratedMRF')
 
--- CRAN/QA/BDR/gannet/packages/updates.R
+++ CRAN/QA/BDR/gannet/packages/updates.R
@@ -14,10 +14,12 @@ Sys.setenv(MOSEKLM_LICENSE_FILE = file.path(mosek, "licenses/mosek.lic"),
 opts <- list(Rserve = "--without-server",
              udunits2 = "--with-udunits2-include=/usr/include/udunits2")
 
-if(grepl("R-clang", R.home()))
+if(grepl("R-clang", R.home())) {
     Sys.setenv(PKG_CONFIG_PATH = '/usr/local/clang/lib64/pkgconfig:/usr/local/lib64/pkgconfig:/usr/lib64/pkgconfig',
                JAGS_LIB = '/usr/local/clang/lib64',
-               PATH=paste("/usr/local/clang/bin", Sys.getenv("PATH"), sep=":"))
+               PATH = paste("/usr/local/clang/bin", Sys.getenv("PATH"), sep=":"))
+    stoplist <- c(stoplist, noinstall_clang)
+}
 
 chooseBioCmirror(ind=1)
 setRepositories(ind = c(1:4))
