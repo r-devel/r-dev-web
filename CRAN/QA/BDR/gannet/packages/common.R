c14173bf4ed151feb4cf81b3974e6b788ad92885
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2016-08-03 15:23:19 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3528 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -1,14 +1,17 @@
 stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RPyGeo',
                'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
-              'RQuantLib', 'RcppAPT', 'caRpools', "ROI.plugin.cplex")
+              'RQuantLib', 'RcppAPT', 'caRpools', "ROI.plugin.cplex",
+	      "CARrampsOcl")
 
 CUDA <- # etc
-c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "Rcplex", "Rhpc", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "localsolver")
+c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "Rcplex", "Rhpc", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "localsolver",
+"OpenCL", "CARrampsOcl", "gpuR")
 
 noclang <- c("RQuantLib", "RcppOctave", "h5", "magick", "pdftools")
 
-noinstall <- c("littler", "gpuR", 'Statomica', 'RSQLServer', 'aemo',
-   'deeplearning', 'seqHMM')
+noinstall <- c("littler", 'Statomica', 'RSQLServer', 'aemo',
+   'qtbase', 'qtpaint', 'qtutils', 'Cyclops',
+   'deeplearning', 'flowDiv', "RcppStreams", "glamlasso")
 
 noinstall_clang <- 
 c('qtbase', 'qtpaint', 'qtutils', "Boom", "BoomSpikeSlab", "bsts",
