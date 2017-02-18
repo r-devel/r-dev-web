3f573be0ce5e33c5279dc7ab3dde2dbf3f97d144
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2017-02-18 12:49:42 +0000

    u
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3623 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -1,7 +1,7 @@
 stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RPyGeo',
                'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
                'RcppAPT', 'caRpools', "ROI.plugin.cplex", "CARrampsOcl",
-               'RQuantLib', 'PharmacoGx', 'IRATER')
+               'RQuantLib', 'PharmacoGx', 'IRATER', "sybilSBML")
 
 CUDA <- # etc
 c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "Rcplex", "Rhpc", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "localsolver",
@@ -10,7 +10,7 @@ c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "
 ## all C++ interfaces to system software
 noclang <- c("RQuantLib", "RcppOctave", "h5", "magick", "sf")
 
-noinstall <- c("littler", "DNAprofiles", "sybilSBML","largeVis", "restfulr")
+noinstall <- c("littler", "DNAprofiles", "largeVis", "restfulr", "Sabermetrics")
 
 noinstall_clang <- c('BAMBI', 'qtbase', 'qtpaint', 'qtutils', 'dfpk')
 
