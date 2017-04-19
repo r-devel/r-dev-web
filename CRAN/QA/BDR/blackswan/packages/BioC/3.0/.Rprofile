6a40a5e52b6f28e9b4e3fa5d7e1fe94764c35557
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2017-04-19 07:51:46 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3659 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/packages/BioC/3.0/.Rprofile
+++
@@ -1,6 +0,0 @@
-Sys.setenv(R_BIOC_VERSION = "3.0")
-options(repos=c(CRAN="file:///data/blackswan/ripley/R"))
-options(BioC_mirror="http://bioconductor.statistik.tu-dortmund.de")
-#options(BioC_mirror="http://mirrors.ebi.ac.uk/bioconductor/")
-utils::setRepositories(ind=2:5)
-.libPaths("~/R/BioC-3.0")
--- CRAN/QA/BDR/blackswan/packages/BioC/3.5/reinstall.R
+++ CRAN/QA/BDR/blackswan/packages/BioC/3.5/reinstall.R
@@ -1,20 +1,20 @@
-#chooseBioCmirror(ind = 6)
-options(BioC_mirror="http://bioconductor.statistik.tu-dortmund.de")
+Sys.setenv(R_BIOC_VERSION="3.5")
+options(BioC_mirror="https://bioconductor.statistik.tu-dortmund.de")
 
 setRepositories(ind=2:5)
 repos <- getOption("repos")
-repos[1] <- "file:///data/blackswan/ripley/R/packages/BioC/3.2"
+repos[1] <- "file:///data/blackswan/ripley/R/packages/BioC/3.5"
 options(repos = repos)
 
 foo <- row.names(installed.packages(.libPaths()[1]))
 
 Sys.setenv(DISPLAY = ':5',
-           TMPDIR = "~/R/packages/BioC/3.2/tmp",
+           TMPDIR = "/data/blackswan/ripley/R/packages/BioC/3.5/tmp",
            RMPI_TYPE = "OPENMPI",
            RMPI_INCLUDE = "/usr/include/openmpi-x86_64",
            RMPI_LIB_PATH = "/usr/lib64/openmpi/lib")
 
-ddir <- '~/R/packages/BioC/3.2/tmp/downloaded_packages'
+ddir <- '~/R/packages/BioC/3.5/tmp/downloaded_packages'
 dir.create(ddir, showWarnings = FALSE)
 install.packages(foo, Ncpus = 30, destdir=ddir)
 unlink(ddir, recursive = TRUE)
