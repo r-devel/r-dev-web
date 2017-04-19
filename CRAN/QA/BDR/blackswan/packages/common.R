c15d4e0e39de4250a00d1a24aa8d9cf6820e9016
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2017-04-19 07:49:58 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3658 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/packages/common.R
+++ CRAN/QA/BDR/blackswan/packages/common.R
@@ -2,16 +2,17 @@ CUDA <-c("CARramps", "HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "
 
 Windows <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RPyGeo',
               'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
-              'rzmq', 'RcppAPT', 'caRpools')
+              'rzmq', 'RcppAPT', 'caRpools', 'RInno')
 stoplist <- c("RcppOctave", "OpenCL", "CARrampsOcl", "gpuR",
               "rLindo", "RQuantLib", "TSMySQL",
 	      "sodium", "homomorpheR", "remoter",
 	      "Rmosek", "REBayes",
 	      "Boom", "BoomSpikeSlab", "bsts",
 	      "littler", "gpuR", "rsvg", "pdftools", "pdfsearch", "textreadr",
-              "multipanelfigure", "magick", "rbi", "IRATER", "tesseract")
+              "multipanelfigure", "magick", "rbi", "IRATER", "tesseract",
+	      "texPreview", "bayesCL")
 
-noinstall <- c("littler", 'RSQLServer', 'deeplearning', "wand", 'DNAprofiles', 'sybilSBML', 'ionicons')
+noinstall <- c("littler", "wand", 'sybilSBML', 'ionicons', 'largeVis', 'restfulr', "plink")
 
 stoplist <- c(stoplist, Windows, CUDA)
 
