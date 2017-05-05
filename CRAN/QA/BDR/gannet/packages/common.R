b6673d96fa95e75205876f47baa77c2a91b32cb6
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2017-05-05 11:02:49 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3668 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -1,17 +1,16 @@
-stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RPyGeo',
-               'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
-	       'RInno',
+stoplist <- c('BiplotGUI', 'MDSGUI', 'R2MLwiN', 'R2PPT', 'R2wd', 'RInno',
+	       'RPyGeo', 'RWinEdt', 'blatr', 'excel.link', 'installr', 'spectrino',
                'RcppAPT', 'caRpools', "ROI.plugin.cplex", "CARrampsOcl",
-               'RQuantLib', 'PharmacoGx', 'IRATER', "sybilSBML")
+               'RQuantLib', 'PharmacoGx', 'IRATER', "sybilSBML", "kmcudaR")
 
 CUDA <- # etc
 c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "Rcplex", "Rhpc", "cplexAPI",  "cudaBayesreg", "gmatrix", "gputools", "magma", "ora", "permGPU", "localsolver",
-"OpenCL", "CARrampsOcl", "gpuR")
+"OpenCL", "CARrampsOcl", "gpuR", "kmcudaR")
 
 ## all C++ interfaces to system software
 noclang <- c("RQuantLib", "RcppOctave", "h5", "magick", "sf", "texPreview")
 
-noinstall <- c("littler", "largeVis", "mcPAFit", "CorReg")
+noinstall <- c("littler", "largeVis", "mcPAFit", "corpus")
 
-noinstall_clang <- c('BAMBI', 'qtbase', 'qtpaint', 'qtutils', 'ManifoldOptim', 'bfp')
+noinstall_clang <- c('BAMBI', 'qtbase', 'qtpaint', 'qtutils', 'ManifoldOptim')
 
