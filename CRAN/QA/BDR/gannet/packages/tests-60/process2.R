fdf2f17235668da43b25d0947057e4080ad6b744
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2017-05-17 16:41:42 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3672 c52295ea-58df-0310-926a-d16021944841

---
+++ CRAN/QA/BDR/gannet/packages/tests-60/process2.R
@@ -0,0 +1,42 @@
+bpath <- "/data/ftp/pub/bdr/memtests/gcc-ASAN"
+Package <- list.dirs(bpath, FALSE, FALSE)
+Versions <- character()
+for(p in Package) {
+    f <- file.path(bpath, p, "00check.log")
+     if(file.exists(f))
+         f <- file.path("/data/gannet/ripley/R/packages/tests-60",
+                        paste0(p, ".out"))
+    ver <- if(file.exists(f)) {
+        ver <- grep("^[*] this is package", readLines(f), value = TRUE)
+        sub(".*version ‘([^’]+)’.*", "\\1", ver)
+    } else NA_character_
+    Versions <- c(Versions, ver)
+}
+href <- paste0("https://www.stats.ox.ac.uk/pub/bdr/memtests/gcc-ASAN/", Package)
+DF <- data.frame(Package = Package, Version = Versions,
+                 kind = rep_len("gcc-ASAN", length(Package)),
+                 href = href, stringsAsFactors = TRUE)
+write.csv(DF, paste0(bpath, ".csv"), row.names = FALSE, quote = FALSE)
+
+bpath <- "/data/ftp/pub/bdr/memtests/gcc-UBSAN"
+files <- list.files(bpath, pattern = "[.]Rout$")
+dirs <- list.dirs(bpath, FALSE, FALSE)
+Package <- c(sub("-Ex[.]Rout$", "", files), dirs)
+Versions <- character()
+for(p in Package) {
+    f <- file.path("/data/gannet/ripley/R/packages/tests-60", paste0(p, ".out"))
+    ver <- if(file.exists(f)) {
+        ver <- grep("^[*] this is package", readLines(f), value = TRUE)
+        sub(".*version ‘([^’]+)’.*", "\\1", ver)
+    } else NA_character_
+    Versions <- c(Versions, ver)
+}
+href <- paste0("https://www.stats.ox.ac.uk/pub/bdr/memtests/gcc-UBSAN/",
+              c(files, dirs))
+
+DF <- data.frame(Package = Package, Version = Versions,
+                 kind = rep_len("gcc-UBSAN", length(Package)),
+                 href = href, stringsAsFactors = TRUE)
+DF <- DF[sort.list(Package), ]
+write.csv(DF, paste0(bpath, ".csv"), row.names = FALSE, quote = FALSE)
+
---
+++ CRAN/QA/BDR/gannet/packages/tests-clang-SAN/process2.R
@@ -0,0 +1,42 @@
+bpath <- "/data/ftp/pub/bdr/memtests/clang-ASAN"
+Package <- list.dirs(bpath, FALSE, FALSE)
+Versions <- character()
+for(p in Package) {
+    f <- file.path(bpath, p, "00check.log")
+     if(file.exists(f))
+         f <- file.path("/data/gannet/ripley/R/packages/tests-clang-SAN",
+                        paste0(p, ".out"))
+    ver <- if(file.exists(f)) {
+        ver <- grep("^[*] this is package", readLines(f), value = TRUE)
+        sub(".*version ‘([^’]+)’.*", "\\1", ver)
+    } else NA_character_
+    Versions <- c(Versions, ver)
+}
+href <- paste0("https://www.stats.ox.ac.uk/pub/bdr/memtests/clang-ASAN/", Package)
+DF <- data.frame(Package = Package, Version = Versions,
+                 kind = rep_len("clang-ASAN", length(Package)),
+                 href = href, stringsAsFactors = TRUE)
+write.csv(DF, paste0(bpath, ".csv"), row.names = FALSE, quote = FALSE)
+
+bpath <- "/data/ftp/pub/bdr/memtests/clang-UBSAN"
+files <- list.files(bpath, pattern = "[.]Rout$")
+dirs <- list.dirs(bpath, FALSE, FALSE)
+Package <- c(sub("-Ex[.]Rout$", "", files), dirs)
+Versions <- character()
+for(p in Package) {
+    f <- file.path("/data/gannet/ripley/R/packages/tests-clang-SAN", paste0(p, ".out"))
+    ver <- if(file.exists(f)) {
+        ver <- grep("^[*] this is package", readLines(f), value = TRUE)
+        sub(".*version ‘([^’]+)’.*", "\\1", ver)
+    } else NA_character_
+    Versions <- c(Versions, ver)
+}
+href <- paste0("https://www.stats.ox.ac.uk/pub/bdr/memtests/clang-UBSAN/",
+              c(files, dirs))
+
+DF <- data.frame(Package = Package, Version = Versions,
+                 kind = rep_len("clang-UBSAN", length(Package)),
+                 href = href, stringsAsFactors = TRUE)
+DF <- DF[sort.list(Package), ]
+write.csv(DF, paste0(bpath, ".csv"), row.names = FALSE, quote = FALSE)
+
