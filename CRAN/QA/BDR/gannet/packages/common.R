314054d7f9409399856493e275a58d1055083d61
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2017-08-01 20:43:27 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3707 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -8,12 +8,55 @@ c("HiPLARM", "RAppArmor", "RDieHarder", "ROI.plugin.cplex", "ROracle", "RSAP", "
 "OpenCL", "CARrampsOcl", "gpuR", "kmcudaR")
 
 ## all C++ interfaces to system software
-noclang <- c("RQuantLib", "RcppOctave", "h5", "magick", "sf", "texPreview",
+noclang <- c("RQuantLib", "RcppOctave", "h5", "magick", "texPreview",
 	     "qtbase", "qtpaint", "qtutils")
 
 no_mosek <- c("REBayes", "Rmosek")
-noinstall <- c("littler", "largeVis", "Rcriticor", "ProjectionBasedClustering")
-
-
+noinstall <- c("littler", "largeVis", "Rcriticor")
 noinstall_clang <- c('BAMBI', 'ManifoldOptim')
 
+#-------------------- functions ---------------------
+
+av <- function()
+{
+    ## setRepositories(ind = 1) # CRAN
+    options(available_packages_filters =
+            c("R_version", "OS_type", "CRAN", "duplicates"))
+    av <- available.packages()[, c("Package", "Version", "Repository")]
+    av <- as.data.frame(av, stringsAsFactors = FALSE)
+    path <- with(av, paste0(Repository, "/", Package, "_", Version, ".tar.gz"))
+    av$Repository <- NULL
+    av$Path <- sub(".*contrib/", "../contrib/", path)
+    av$mtime <- file.info(av$Path)$mtime
+    av[order(av$Package), ]
+}
+
+### NB: this assumes UTF-8 quotes
+get_vers <- function(nm) {
+    ## read already-checked versions
+    vers <- sapply(nm, function(n) {
+        if (file.exists(f <- paste0(n, ".out"))) {
+            ver <- grep("^[*] this is package", readLines(f, warn = FALSE),
+                        value = TRUE,  useBytes = TRUE)
+            if(length(ver)) sub(".*version ‘([^’]+)’.*", "\\1", ver) else "10000.0.0"
+        } else "10000.0.0"
+    })
+    package_version(vers)
+}
+
+do_it <- function(stoplist) {
+    tars <-  av()
+    tars <- tars[!tars$Package %in% stoplist, ]
+    nm <- tars$Package
+    time0 <- file.info(paste0(nm, ".in"))$mtime
+#    vers <- get_vers(nm)
+    unpack <- is.na(time0) | (tars$mtime > time0) #| (tars$Version > vers)
+    for(i in which(unpack)) {
+        if(nm[i] %in% stoplist) next
+        cat(nm[i], "\n", sep = "")
+        unlink(nm[i], recursive = TRUE)
+        unlink(paste0(nm[i], ".out"))
+        system(paste("tar -zxf", tars[i, "Path"]))
+        system(paste("touch -r", tars[i, "Path"], paste0(nm[i], ".in")))
+    }
+}
