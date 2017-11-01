0945f42b83e80d1099cdeecad36a78ecead2b1d0
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2017-11-01 08:26:12 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3758 c52295ea-58df-0310-926a-d16021944841

---
+++ CRAN/QA/BDR/blackswan/packages/BioC/3.7/reinstall.R
@@ -0,0 +1,20 @@
+Sys.setenv(R_BIOC_VERSION="3.7")
+#options(BioC_mirror="https://bioconductor.statistik.tu-dortmund.de")
+
+setRepositories(ind=2:4, 7)
+repos <- getOption("repos")
+repos[1] <- "file:///data/blackswan/ripley/R/packages/BioC/3.7"
+options(repos = repos)
+
+foo <- row.names(installed.packages(.libPaths()[1]))
+
+Sys.setenv(DISPLAY = ':5',
+           TMPDIR = "/data/blackswan/ripley/R/packages/BioC/3.6/tmp",
+           RMPI_TYPE = "OPENMPI",
+           RMPI_INCLUDE = "/usr/include/openmpi-x86_64",
+           RMPI_LIB_PATH = "/usr/lib64/openmpi/lib")
+
+ddir <- '~/R/packages/BioC/3.7/tmp/downloaded_packages'
+dir.create(ddir, showWarnings = FALSE)
+install.packages(foo, Ncpus = 30, destdir=ddir)
+unlink(ddir, recursive = TRUE)
