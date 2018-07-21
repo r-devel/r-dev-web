d8bf09f3fed391aae27df058b24c3b45d7270809
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2018-07-21 07:15:21 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@3901 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -15,8 +15,11 @@ c("RDieHarder", "ROI.plugin.cplex", "ROracle", "Rcplex", "Rhpc", "cplexAPI",  "c
 noclang <- c("RQuantLib", "RcppOctave", "qtbase", "qtpaint", "qtutils")
 
 #no_mosek <- c("REBayes", "Rmosek")
-noinstall <- c("littler", 'FastRWeb', 'archivist.github', 'fungible', 'psychmeta') #, 'ReporteRs', 'ReporteRsjars', 'ITGM', 'fdq', 'Fgmutils', 'microplot', 'RtutoR', 'rtable')
+noinstall <- c("littler", 'archivist.github', 'psychmeta', 'dGAselID', 'nlcv')
 noinstall_clang <- c('ManifoldOptim', 'flowDiv', 'CEC', 'PythonInR', 'IGP', 'rlo', 'WebGestaltR')
+
+if(grepl("/MKL", R.home()))
+    noinstall <- c(noinstall, 'plsgenomics', 'MAclinical', 'specmine')
 
 #-------------------- functions ---------------------
 
