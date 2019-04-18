83dde44da1e8450210baac172e2851aaa33e925d
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2019-04-18 08:01:36 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4044 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -16,7 +16,7 @@ stoplist <- c(CUDA,
 ## all C++ interfaces to external software
 noclang <- c("RQuantLib", "opencv", "qtbase", "qtpaint", "qtutils")
 
-noinstall <- c("deisotoper", "glmulti", "DiversityOccupancy", "vortexR",
+noinstall <- c("deisotoper", #"glmulti", "DiversityOccupancy", "vortexR",
 	       "madness", "rjmcmc",
   "gdsfmt", "SNPRelate", "GMMAT", 'Mega2R', 'EthSEQ', 'dartR', "R.SamBada")
 noinstall_clang <- readLines('~/R/packages/mzR_fail') #c()
