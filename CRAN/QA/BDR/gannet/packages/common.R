a8d209a0e32a299296990adb4bbe1a944ccc901a
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2019-05-29 10:35:55 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4069 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -19,7 +19,8 @@ noclang <- c("RQuantLib", "opencv", "qtbase", "qtpaint", "qtutils")
 
 noinstall <- c("RDS", "sspse")
 #  "gdsfmt", "SNPRelate", "GMMAT", 'Mega2R', 'EthSEQ', 'dartR', "R.SamBada")
-noinstall_clang <- c(readLines('~/R/packages/mzR_fail')) #c()
+OMx <- c('OpenMx', 'EasyMx', 'MBESS', 'ctsem', 'ifaTools', 'metaSEM', 'semPlot', 'semtree', 'umx', 'EGAnet', 'MOTE', 'auRoc', 'iMediate', 'lavaan.shiny', 'lvnet', 'predictionInterval', 'replicationInterval', 'semdrw', 'userfriendlyscience', 'behaviorchange', 'codebook', 'rosetta')
+noinstall_clang <- c("mssm", readLines('~/R/packages/mzR_fail')) #c()
 noinstall_pat <- c()
 
 #-------------------- functions ---------------------
