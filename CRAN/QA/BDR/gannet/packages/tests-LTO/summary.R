3453a0d73a075ae8484027aadeacb5ce03064f6d
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2019-07-10 07:50:18 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4079 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/tests-LTO/summary.R
+++ CRAN/QA/BDR/gannet/packages/tests-LTO/summary.R
@@ -3,7 +3,7 @@ dest <- "/data/ftp/pub/bdr/LTO"
 files <- dir(patt = "[.]out$")
 for(f in files) {
    d <- file.path(dest, f)
-   if(any(grepl("-Wlto", readLines(f), useBytes = TRUE)))
+   if(any(grepl("-W(lto|odr)", readLines(f), useBytes = TRUE)))
 	file.copy(f, dest, overwrite = TRUE, copy.date = TRUE)
    else if (file.exists(d)) file.remove(d)
 }
--- CRAN/QA/BDR/gannet/packages/tests-LTO2/summary.R
+++ CRAN/QA/BDR/gannet/packages/tests-LTO2/summary.R
@@ -3,7 +3,7 @@ dest <- "/data/ftp/pub/bdr/LTO2"
 files <- dir(patt = "[.]out$")
 for(f in files) {
    d <- file.path(dest, f)
-   if(any(grepl("(-Wlto|multiple definition of)", readLines(f), useBytes = TRUE)))
+   if(any(grepl("(-Wlto|-Wodr|multiple definition of)", readLines(f), useBytes = TRUE)))
 	file.copy(f, dest, overwrite = TRUE, copy.date = TRUE)
    else if (file.exists(d)) file.remove(d)
 }
