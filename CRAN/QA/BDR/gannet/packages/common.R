f0e5d099cad20789487bc4f406d597841bc6cf0c
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2019-08-30 05:45:11 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4105 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/common.R
+++ CRAN/QA/BDR/gannet/packages/common.R
@@ -18,7 +18,7 @@ stoplist <- c(CUDA,
 noclang <- c("RQuantLib", "opencv", "qtbase", "qtpaint", "qtutils")
 
 noinstall <- c()
-noinstall_clang <- c('rmumps')
+noinstall_clang <- c('symmetry')
 noinstall_pat <- c()
 
 #-------------------- functions ---------------------
