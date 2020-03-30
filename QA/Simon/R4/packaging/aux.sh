7f44092a06b4b9ce6ac7f0277c1431fa7ff5456a
urbaneks <urbaneks@c52295ea-58df-0310-926a-d16021944841>
2020-03-30 03:53:14 +0000

    add README, nightly and packaging/aux.sh
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4204 c52295ea-58df-0310-926a-d16021944841

---
+++ QA/Simon/R4/packaging/aux.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# aux.sh [--force] 
+
+PACKAGES="texinfo-6.7 tcltk-8.6:8.6.6"
+
+: ${BASE=/Volumes/Builds/R4}
+
+PKGROOT="$BASE/packaging"
+
+cd "$PKGROOT"
+
+. $BASE/unlock-sign
+
+for pkg in $PACKAGES; do
+    name=`echo $pkg | sed 's:-.*::'`
+    dir=`echo $pkg | sed 's/:.*//'`
+    ver=`echo $pkg | sed 's:.*-::' | sed 's/.*://'`
+    echo ''
+    echo "--- Packaging $name in $dir, version $ver ..."
+    echo ''
+
+    if [ ! -e $PKGROOT/$dir/usr ]; then
+	echo "*** ERROR: missing $PKGROOT/$dir/usr" >&2
+	exit 1
+    fi
+    if [ ! -e "$PKGROOT/$name.plist" ]; then
+	echo "*** ERROR: missing $PKGROOT/$name.plist" >&2
+	echo " (you can use pkgbuild --analyze --root $dir $name.plist to create a template)"
+	exit 1
+    fi
+    echo " - signing contents of $dir"
+    $PKGROOT/R-sign-contents texinfo-6.7 "$1"
+
+    echo " - building $name.pkg from $dir ..."
+    if pkgbuild --sign 'Developer ID Installer' --identifier org.r-project.x86_64.texinfo --install-location / --version "$ver" --timestamp --root "$dir" --component-plist "$name.plist" "$name.pkg"; then
+	echo "   OK"
+	ls -l "$name.pkg"
+    else
+	echo "*** ERROR: failed: pkgbuild --sign 'Developer ID Installer' --identifier org.r-project.x86_64.texinfo --install-location / --version $ver --timestamp --root $dir --component-plist $name.plist $name.pkg"
+	exit 1
+    fi
+done
+
