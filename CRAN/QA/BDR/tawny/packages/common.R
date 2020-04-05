433fe45916074e300524ddd9d6be95e680d678d7
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2020-04-05 15:39:48 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4208 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/tawny/packages/common.R
+++ CRAN/QA/BDR/tawny/packages/common.R
@@ -1,49 +1,36 @@
 stoplist <-
-    c("BRugs", "RAppArmor",
-      "RMark", "ROracle", "RQuantLib",
-      "RSAP", "Rcplex", "RcppOctave", "Rpoppler", "ora", "rLindo",
-      "ROI.plugin.cplex",
-      ## CUDA
-      "CARramps", "HiPLARM", "WideLM", "cplexAPI", "cudaBayesreg", "gmatrix",
-      "gputools",  'iFes', "localsolver", "magma", "permGPU", "rpud", 'kmcudaR',
-      "gpda", "gpuR",
-      ## qt users
-      "qtbase", "qtpaint", "qtutils", "VBmix",
-      "RcppAPT", 
+    c("BRugs",
+      "ROracle", "RQuantLib",
+      "Rcplex", "Rpoppler", "ora", "rLindo", "ROI.plugin.cplex", "cplexAPI",
+      "localsolver", "permGPU", 'kmcudaR', "gpuR",
+      "qtbase", "qtpaint", ## qt users
       "rcrypt", # needs gpg
-      #"magick", "texPreview", # need C++ interface
       ## ggobi no longer builds, binary is linked to old GTK+
       "rggobi", "PKgraph", "SeqGrapheR", "beadarrayMSV", "clusterfly",
       "gpg", "IRATER", "tesseract",
-      'argparse', 'optparse', # missing SystemRequirements
-      'rpg', 'mssqlR',
-      'RmecabKo',
-      'opencv',
-      'ecmwfr', ## never returns
-      'arrow',
+#      'argparse', 'optparse', # missing SystemRequirements
+      'mssqlR',
+      'opencv', # needs opencv4
+      #'ecmwfr', ## never returns
+      #'arrow',
       ## memory issues
       'cbq', 'ctsem', 'pcFactorStan',
       ## external tools
       #"PythonInR", "IGP", "WebGestaltR", "rlo",
-      "RAppArmor", "RcppAPT", "RcppMeCab", "Rgretl", "RmecabKo",
-      "RMLwiN", 
+      "RAppArmor", "RcppAPT", "RcppMeCab", "RmecabKo",
       "RMark", "R2ucare", "multimark",
-      'Rsagacmd',
-      'RPostgres', 'RGreenplum',
+      #'Rsagacmd',
+      'RPostgres', 'RGreenplum', "rpg",
       "caRpools", # MAGeCK
-      'msgtools', # no GNU gettext
       "nFCA", # ruby
-      "rsvg", "ChemmineR", "RIdeogram", "colorfindr", "netSEM", "uCAREChemSuiteCLI", "vtree", "integr", "cohorttools", "BioMedR",
+      "rsvg", "RIdeogram", "colorfindr", "netSEM", "uCAREChemSuiteCLI", "vtree", "integr", "cohorttools", "BioMedR",
       "sybilSBML", "rGEDI",
-      'OpenCL', 'bayesCL',
-      'rrd', # need rrdtool libraries
-      "tmuxr")
+      'OpenCL',
+      'rrd') # need rrdtool libraries
 
-C8 <- readLines('~/R/packages/mzR_fail')
+## nothing depends on mzR any more
+#C8 <- readLines('~/R/packages/mzR_fail')
 
 noinstall <- c('rMouse', # AWT headless
-               'AntMAN', 'ChannelAttribution', 'GFORCE', 'KRIG', 'LBLGXE', 'atakrig', 'clrdag', 'durmod', 'mirtjml', 'mpMap2', 'mrfDepth', 'mrfse', 'qgg', "rMVP", 'spNNGP', 'support',
-"ChannelAttributionApp", "bivrp", "rospca",      
-               "anomaly", "ggtern", "cocktailApp", "plot3logit", "tricolore",
-               #'RVowpalWabbit', # 'thread' conflict
-	       'cyanoFilter','flowDiv', 'beadplexr')
+               "ggtern", "cocktailApp", "plot3logit", "tricolore",
+	       'cyanoFilter','flowDiv') # cytolib
