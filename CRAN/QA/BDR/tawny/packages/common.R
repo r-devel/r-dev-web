76ef556bbc53c120238270b73cdb052c650a8a1d
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2020-04-26 15:20:42 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4231 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/tawny/packages/common.R
+++ CRAN/QA/BDR/tawny/packages/common.R
@@ -1,39 +1,22 @@
 stoplist <-
     c("BRugs",
-      "ROracle", "RQuantLib",
-      "Rcplex", "Rpoppler", "ora", "rLindo", "ROI.plugin.cplex", "cplexAPI",
+      "ROracle", "RQuantLib", "ora",
+      "Rcplex", "Rpoppler", "rLindo", "ROI.plugin.cplex", "cplexAPI",
       "localsolver", "permGPU", 'kmcudaR', "gpuR",
       "qtbase", "qtpaint", ## qt users
-      "rcrypt", # needs gpg
       ## ggobi no longer builds, binary is linked to old GTK+
       "rggobi", "PKgraph", "SeqGrapheR", "beadarrayMSV", "clusterfly",
-      "gpg", "IRATER",# "tesseract",
-#      'argparse', 'optparse', # missing SystemRequirements
+      "IRATER", # ADMB
       'mssqlR',
-#      'opencv', # needs opencv4
-      #'gert', # builds if brew is not on the path!
-      #'ecmwfr', ## never returns
-      #'arrow',
       ## memory issues
       'cbq', 'ctsem', 'pcFactorStan',
       ## external tools
-      #"PythonInR", "IGP", "WebGestaltR", "rlo",
       "RAppArmor", "RcppAPT", "RcppMeCab", "RmecabKo",
       "RMark", "R2ucare", "multimark",
-      #'Rsagacmd',
-      "rpg",
-#      'RPostgres', 'RGreenplum',
+      "rpg", # PostgreSQL
       "caRpools", # MAGeCK
-#      "nFCA", # ruby
-#      "rsvg", "RIdeogram", "colorfindr", "netSEM", "uCAREChemSuiteCLI", "vtree", "integr", "cohorttools", "BioMedR",
       "sybilSBML", "rGEDI",
       'OpenCL',
       'rrd') # need rrdtool libraries
 
-## nothing depends on mzR any more
-#C8 <- readLines('~/R/packages/mzR_fail')
-
-noinstall <- c('rMouse', # AWT headless
-               "tricolore",
-               "SpatialPosition", "edbuildmapr", "tanaka", "tolBasis") 
-	       #'cyanoFilter','flowDiv') # cytolib
+noinstall <- c("tricolore", 'systemfonts')
