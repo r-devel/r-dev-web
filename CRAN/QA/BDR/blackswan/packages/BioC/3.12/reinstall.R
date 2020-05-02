cde27f4d6b640b9f461cbc64a4a4e4ce133d7697
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2020-05-02 08:55:00 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4239 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/packages/BioC/3.12/reinstall.R
+++ CRAN/QA/BDR/blackswan/packages/BioC/3.12/reinstall.R
@@ -1,10 +1,9 @@
-Sys.setenv(R_BIOC_VERSION="3.9")
-#options(BioC_mirror="https://bioconductor.statistik.tu-dortmund.de")
+Sys.setenv(R_BIOC_VERSION="3.12")
 options(BioC_mirror="http://bioconductor.org")
 
 setRepositories(ind=2:4)
 repos <- getOption("repos")
-repos[1] <- "file:///data/blackswan/ripley/R/packages/BioC/3.9"
+repos[1] <- "file:///data/blackswan/ripley/R/packages/BioC/3.12"
 options(repos = repos)
 
 args <- commandArgs()[-(1:3)]
@@ -17,12 +16,12 @@ foo <- if(la <- length(args)) {
 #foo <- setdiff(foo, "Rhdf5lib")
 
 Sys.setenv(DISPLAY = ':5',
-           TMPDIR = "/data/blackswan/ripley/R/packages/BioC/3.9/tmp",
+           TMPDIR = "/data/blackswan/ripley/R/packages/BioC/3.12/tmp",
            RMPI_TYPE = "OPENMPI",
            RMPI_INCLUDE = "/usr/include/openmpi-x86_64",
            RMPI_LIB_PATH = "/usr/lib64/openmpi/lib")
 
-ddir <- '~/R/packages/BioC/3.9/tmp/downloaded_packages'
+ddir <- '~/R/packages/BioC/3.12/tmp/downloaded_packages'
 dir.create(ddir, showWarnings = FALSE)
 install.packages(foo, Ncpus = 10, destdir=ddir)
 unlink(ddir, recursive = TRUE)
