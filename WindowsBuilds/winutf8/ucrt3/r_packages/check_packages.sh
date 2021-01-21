f3f42679114d563ec49a8dd234768451633cce73
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-01-21 21:22:37 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4418 c52295ea-58df-0310-926a-d16021944841

---
+++ WindowsBuilds/winutf8/ucrt3/r_packages/check_packages.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+# Check all CRAN packages from a local mirror. Expects an installed library
+# of packages in pkgcheck/lib. R has to be on PATH.
+
+JOBS=40
+
+# -----
+
+SELF="$0"
+UHOME=`pwd`
+
+if [ "$#" == 0 ] ; then
+  # uses GNU parallel
+  #   checking of BIOC packages can be enabled here
+  parallel -j $JOBS -n 20 $SELF -- $UHOME/mirror/CRAN/src/contrib/*.tar.gz
+
+  # generate reports
+  KIND=gcc10-UCRT
+  URL=https://www.r-project.org/nosvn/winutf8/ucrt3
+
+  for REPO in CRAN BIOC ; do
+    RD=$UHOME/pkgcheck/results/$REPO/checks/$KIND
+    mkdir -p $RD
+    D=$UHOME/pkgcheck/$REPO
+
+    if [ -d $D ] ; then
+      cd $D
+      echo Package,Version,kind,href >$RD/$KIND.csv
+      egrep -l '(ERROR|WARNING)$' */*.out | cut -d/ -f1 | \
+        while read P ; do
+          VER=`grep "^* this is package.*version" $P/$P.out  | sed -e 's/.*version //g' | tr -d \'`
+          echo $P,$VER,$KIND,$URL/$REPO/checks/$KIND/$P/$P.out >>$RD/$KIND.csv
+          mkdir -p $RD/$P
+          cp $P/$P.out $RD/$P 
+        done
+    fi
+  done
+
+  exit 0
+fi
+
+# recursively invoked to check specific packages
+
+export R_BROWSER=false
+export R_PDFVIEWER=false
+export _R_CHECK_PKG_SIZES_=false
+export _R_CHECK_FORCE_SUGGESTS_=false
+export _R_CHECK_ELAPSED_TIMEOUT_=1800
+export R_LIBS=`pwd`/pkgcheck/lib
+
+shift
+for SRC in $* ; do
+  PKG=`echo $SRC | sed -e 's/.*\/\([^_]*\)_.*$/\1/g'`
+  REPO=`echo $SRC | sed -E -e 's/.*\/(CRAN|BIOC)\/.*/\1/g'`
+
+  CDIR=$UHOME/pkgcheck/$REPO/$PKG
+  mkdir -p $CDIR
+  export TMPDIR=$CDIR/tmp
+  mkdir -p $TMPDIR
+  cd $CDIR
+  set > env.log
+  R CMD check $SRC >$PKG.out 2>&1
+done
+
