8e7a0f6fdc52fdd16ec49aeb0403c30192957f08
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-01-21 21:25:48 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4419 c52295ea-58df-0310-926a-d16021944841

---
+++ WindowsBuilds/winutf8/ucrt3/r_packages/install_packages_for_checking.r
@@ -0,0 +1,88 @@
+
+# Install all packages to create a library for checking packages against
+# it. Re-use binary versions from "build" as normally this is run just
+# after updating the local mirror and building packages, hence re-use
+# will reduce network traffic.
+
+# --- customize below
+
+#CRAN_mirror <- "https://cran.r-project.org"
+#BIOC_mirror <- "https://master.bioconductor.org/packages/3.12"
+
+CRAN_mirror <- paste0("file:///", getwd(), "/mirror/CRAN")
+BIOC_mirror <- paste0("file:///", getwd(), "/mirror/BIOC")
+
+CRAN_bins <- paste0("file:///", getwd(), "/build/CRAN")
+BIOC_bins <- paste0("file:///", getwd(), "/build/BIOC")
+
+onlycran <- TRUE # only CRAN packages will be explicitly installed
+                 # some BIOC dependencies will be installed implicitly
+Ncpus <- 20
+
+# --- customize above
+
+# binary packages (most CRAN, few BIOC dependencies, local mirror)
+repos_bin <- c(CRAN_bins, BIOC_bins)
+
+repos_src <- c(CRAN_mirror,
+        paste0(BIOC_mirror, "/bioc"),
+        paste0(BIOC_mirror, "/data/annotation"),
+        paste0(BIOC_mirror, "/data/experiment"),
+        paste0(BIOC_mirror, "/workflows"))
+
+curls_src <- paste0(repos_src, "/src/contrib")
+           
+mkdir <- function(d)
+  if (!dir.exists(d))
+    dir.create(d, recursive = TRUE)
+
+mkdir("pkgcheck/lib")
+mkdir("pkgcheck/install_out")
+owd <- setwd("pkgcheck/install_out")
+
+ap <- available.packages(curls_src)
+if (onlycran) {
+  iscran <- grepl(CRAN_mirror, ap[,"Repository"])
+  toinst <- ap[iscran, "Package"]
+} else 
+  toinst <- ap[, "Package"]
+
+toinst_last <- -1
+iter <- 1
+
+repos <- c(repos_bin, repos_src)
+libdir <- paste0(owd, "/pkgcheck/lib")
+
+options(install.packages.check.source = "no")
+options(install.packages.compile.from.source = "yes")
+
+while(TRUE) {
+  ip <- installed.packages(lib.loc=libdir)[,"Package"]
+  toinst <- toinst[ !(toinst %in% ip) ]
+
+  cat("Iteration", iter, "packages left to install",
+      length(toinst), "already installed", length(ip), "\n")
+
+  if (length(toinst) == 0) {
+    cat("All packages were installed.\n")
+    break # unlikely
+  }
+  if (length(toinst) == toinst_last) {
+    cat("No new packages were installed in last iteration.\n")
+    break
+  }
+  if (iter > 10) {
+    cat("Maximum number of iterations reached.\n")
+    break
+  }
+
+  cat("Iteration", iter, "installing packages...\n")
+  toinst_last <- length(toinst)
+  iter <- iter + 1
+
+  install.packages(pkgs=toinst, repos=repos, Ncpus=Ncpus, 
+    keep_outputs=TRUE, type="both", lib=libdir)
+}
+
+setwd(owd)
+
