2377366883b6190088306b819e76558a06312a97
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-03-02 14:03:02 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4473 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/imagemagick.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/imagemagick.mk
@@ -8,7 +8,8 @@ $(PKG)_VERSION  := 7.0.8-63
 $(PKG)_CHECKSUM := 5dda18f70662015d7cc89d6d3699cb14def23ad3d5066a43a50ec222e579884f
 $(PKG)_GH_CONF  := ImageMagick/ImageMagick/tags
 $(PKG)_DEPS     := cc bzip2 ffmpeg fftw freetype jasper jpeg lcms \
-                   liblqr-1 libltdl libpng libraw openexr pthreads tiff zlib
+                   liblqr-1 libltdl libpng libraw openexr pthreads tiff zlib \
+                   librsvg
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
@@ -18,6 +19,10 @@ define $(PKG)_BUILD
         --with-freetype='$(PREFIX)/$(TARGET)/bin/freetype-config' \
         --with-utilities=no \
         --with-zero-configuration \
+        --with-rsvg \
+        --with-freetype \
+        --with-cairo \
+        --without-fontconfig \
         LIBS="`'$(TARGET)-pkg-config' --libs libtiff-4`"
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' bin_PROGRAMS=
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install bin_PROGRAMS=
