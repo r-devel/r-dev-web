5d7dce0ea2305abe5d78335d38d74b66eef403a2
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-08-12 15:15:31 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4688 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/settings.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/settings.mk
@@ -7,12 +7,12 @@ MXE_TARGETS := x86_64-w64-mingw32.static.posix
 #  --- base toolchain, plus libraries for base and recommended R packages ---
 
 # tcl tk tktable
-LOCAL_PKG_LIST := bzip2 cairo curl fontconfig freetype gcc icu4c jpeg libpng ncurses openssl pcre2 pixman readline tiff xz zlib librtmp
+LOCAL_PKG_LIST := bzip2 cairo curl fontconfig freetype gcc icu4c jpeg libpng ncurses openssl pcre2 pixman readline tiff xz zlib librtmp zstd
 LOCAL_PKG_LIST += gcc-host
 
 #  --- libraries for other contributed R packages, development tools ---
 
-LOCAL_PKG_LIST += binutils boost cfitsio cmake expat ffmpeg fftw gdal geos gettext giflib glpk gmp gpgme gsl harfbuzz hdf4 hdf5 isl jasper jsoncpp lame lapack libarchive libassuan libffi libgeotiff libgit2 libiconv libsndfile libssh libssh2 libtool libuv libvpx libwebp libxml2 lz4 mpc mpfr nasm netcdf nettle nlopt openblas opencv pcre poppler proj protobuf tcl termcap x264 xvidcore yasm zstd intel-tbb json-c
+LOCAL_PKG_LIST += binutils boost cfitsio cmake expat ffmpeg fftw gdal geos gettext giflib glpk gmp gpgme gsl harfbuzz hdf4 hdf5 isl jasper jsoncpp lame lapack libarchive libassuan libffi libgeotiff libgit2 libiconv libsndfile libssh libssh2 libtool libuv libvpx libwebp libxml2 lz4 mpc mpfr nasm netcdf nettle nlopt openblas opencv pcre poppler proj protobuf tcl termcap x264 xvidcore yasm intel-tbb json-c
 LOCAL_PKG_LIST += freexl gpgme ogg spatialite tre vorbis yaml-cpp jsoncpp lzo openjpeg pkgconf sqlite libgit2
 LOCAL_PKG_LIST += imagemagick librsvg libmysqlclient sox gtk2 libzmq
 
