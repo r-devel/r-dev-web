981e845cd5ca6266278fa5b51063c9db1703f982
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-08-30 20:06:06 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4709 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/redland.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/redland.mk
@@ -7,13 +7,17 @@ $(PKG)_CHECKSUM := de1847f7b59021c16bdc72abb4d8e2d9187cd6124d69156f3326dd34ee043
 $(PKG)_SUBDIR   := redland-$($(PKG)_VERSION)
 $(PKG)_FILE     := redland-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://download.librdf.org/source/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc expat rasqal raptor2
+$(PKG)_DEPS     := cc expat rasqal raptor2 postgresql
 
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --with-mysql=no \
         --with-bdb=no \
+        --with-sqlite='$(PREFIX)/$(TARGET)/include' \
+        --with-postgresql='$(PREFIX)/$(TARGET)/bin/pg_config' \
+        --with-threestore=no \
+        --with-virtuoso=no \
         CFLAGS="$(CFLAGS) $(if $(BUILD_STATIC),-DRASQAL_STATIC -DRAPTOR_STATIC -DREDLAND_STATIC)"
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
 endef
