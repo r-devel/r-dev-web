dac1b60d7e70f6535791fb998706e1253f1176e8
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-08-31 21:45:54 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4717 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/build.sh
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/build.sh
@@ -20,21 +20,24 @@ cd usr
 #
 # tar cfh - x86_64-w64-mingw32.static.posix | xz -T${CPUS} -9ve > ../../build/gcc10_ucrt3.txz
 
-tar --exclude="*-tests" --exclude="test*.exe"     --exclude="*gdal*.exe" \
-  --exclude="*rtmp*.exe" --exclude="*gnutls*.exe" --exclude="hb-*.exe" \
-  --exclude="ogr*.exe" --exclude="certtool.exe" --exclude="gnmmanage.exe" \
-  --exclude="nearblack.exe"     --create --dereference --file - \
-  x86_64-w64-mingw32.static.posix | \
+find x86_64-w64-mingw32.static.posix -printf "%k %p\n" | sort -n | cut -d' ' -f2 | \
+  tar --exclude="*-tests" --exclude="test*.exe"     --exclude="*gdal*.exe" \
+    --exclude="*rtmp*.exe" --exclude="*gnutls*.exe" --exclude="hb-*.exe" \
+    --exclude="ogr*.exe" --exclude="certtool.exe" --exclude="gnmmanage.exe" \
+    --exclude="nearblack.exe" \
+    --create --dereference --no-recursion --files-from - --file - | \
   zstd -T0 -22 --ultra > ../../build/gcc10_ucrt3.tar.zst
 
-# zstd has better compression ratio, but keep this at least for now as not to break
-# existing scripts  
-tar --exclude="*-tests" --exclude="test*.exe"     --exclude="*gdal*.exe" \
-  --exclude="*rtmp*.exe" --exclude="*gnutls*.exe" --exclude="hb-*.exe" \
-  --exclude="ogr*.exe" --exclude="certtool.exe" --exclude="gnmmanage.exe" \
-  --exclude="nearblack.exe"     --create --dereference --file - \
-  x86_64-w64-mingw32.static.posix | \
-  xz -T${CPUS} -9ve > ../../build/gcc10_ucrt3.txz
+# zstd has a slightly better compression ratio on these files, but keep xz
+# at least for now as not to break existing scripts
 
-tar cfh - `ls -1 | grep -v x86_64-w64-mingw32.static.posix` | xz -T${CPUS} -9ve > ../../build/gcc10_ucrt3_cross.txz
+find x86_64-w64-mingw32.static.posix -printf "%k %p\n" | sort -n | cut -d' ' -f2 | \
+  tar --exclude="*-tests" --exclude="test*.exe"     --exclude="*gdal*.exe" \
+    --exclude="*rtmp*.exe" --exclude="*gnutls*.exe" --exclude="hb-*.exe" \
+    --exclude="ogr*.exe" --exclude="certtool.exe" --exclude="gnmmanage.exe" \
+    --exclude="nearblack.exe" \
+    --create --dereference --no-recursion --files-from - --file - | \
+  xz -T0 -9ve > ../../build/gcc10_ucrt3.txz
+
+tar cfh - `ls -1 | grep -v x86_64-w64-mingw32.static.posix` | xz -T0 -9ve > ../../build/gcc10_ucrt3_cross.txz
 cd ../..
