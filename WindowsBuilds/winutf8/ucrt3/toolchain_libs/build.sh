09c5acaeb0038d653c377bc0753b1d633ec75e70
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-09-01 14:46:45 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4719 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/build.sh
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/build.sh
@@ -10,29 +10,47 @@ CPUS=`cat /proc/cpuinfo | grep ^physical.*0 | wc -l`
 
 mkdir build
 cd mxe
-make -j ${CPUS} 2>&1 | tee make.out
-cp make.out ../build
+rm -rf "tmp-*"
 
-cd usr
+# build base and full toolchain
+#   for simplicity these are separate builds, but the ccache is shared
+#   the builds are left in usr_base and usr_full, and move to usr
+#     for the actual builds
+for TYPE in base full ; do
+  rm -rf usr
+  if [ -d usr_${TYPE} ] ; then
+    mv usr_${TYPE} usr
+  fi
+  make -j ${CPUS} R_TOOLCHAIN_TYPE=${TYPE} 2>&1 | tee make_${TYPE}.out
+  cp make_${TYPE}.out ../build
+  
+  # zstd has a slightly better compression ratio on these files, but not
+  # substantially for justifying a change in file names, etc
+  #   tried with "zstd -T0 -22 --ultra > ../../build/gcc10_ucrt3.tar.zst"
+  #
+  # with both zstd and xz it significantly helps to archive files in the order
+  # of file size, so that duplicated executables and executables linked to the
+  # same libraries are more likely close in the archive
+  #
+  # excluding executables, particularly tests, which are not needed by most users,
+  # but they take a lot of space because of static linking
 
-# zstd has a slightly better compression ratio on these files, but not
-# substantialy for justifying a change in file names, etc
-#   tried with "zstd -T0 -22 --ultra > ../../build/gcc10_ucrt3.tar.zst"
-#
-# with both zstd and xz it significantly helps to archive files in the order
-# of file size, so that duplicated executables and executables linked to the
-# same libraries are more likely close in the archive
-#
-# excluding executables, particularly tests, which are not needed by most users,
-# but they take a lot of space because of static linking
+  cd usr
+  find x86_64-w64-mingw32.static.posix -printf "%k %p\n" | sort -n | cut -d' ' -f2 | \
+    tar --exclude="*-tests" --exclude="test*.exe" --exclude="*gdal*.exe" \
+      --exclude="*rtmp*.exe" --exclude="*gnutls*.exe" --exclude="hb-*.exe" \
+      --exclude="ogr*.exe" --exclude="certtool.exe" --exclude="gnmmanage.exe" \
+      --exclude="nearblack.exe" \
+      --create --dereference --no-recursion --files-from - --file - | \
+    xz -T0 -9ve > ../../build/gcc10_ucrt3_${TYPE}.txz
 
-find x86_64-w64-mingw32.static.posix -printf "%k %p\n" | sort -n | cut -d' ' -f2 | \
-  tar --exclude="*-tests" --exclude="test*.exe"     --exclude="*gdal*.exe" \
-    --exclude="*rtmp*.exe" --exclude="*gnutls*.exe" --exclude="hb-*.exe" \
-    --exclude="ogr*.exe" --exclude="certtool.exe" --exclude="gnmmanage.exe" \
-    --exclude="nearblack.exe" \
-    --create --dereference --no-recursion --files-from - --file - | \
-  xz -T0 -9ve > ../../build/gcc10_ucrt3.txz
+  tar cfh - `ls -1 | grep -v x86_64-w64-mingw32.static.posix` | \
+    xz -T0 -9ve > ../../build/gcc10_ucrt3_${TYPE}_cross.txz
+  cd ..
+  mv usr usr_${TYPE}
+done
 
-tar cfh - `ls -1 | grep -v x86_64-w64-mingw32.static.posix` | xz -T0 -9ve > ../../build/gcc10_ucrt3_cross.txz
-cd ../..
+cd ..
+
+# rename for back-compatibility
+mv build/gcc10_ucrt3_full.txz build/gcc10_ucrt3.txz
--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/settings.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/settings.mk
@@ -9,18 +9,25 @@ MXE_TARGETS := x86_64-w64-mingw32.static.posix
 # tcl tk tktable not included (built separately as Tcl/Tk bundle, for
 # historical reasons)
 
-LOCAL_PKG_LIST := bzip2 cairo curl fontconfig freetype gcc icu4c jpeg libpng ncurses openssl pcre2 pixman readline tiff xz zlib librtmp zstd
-LOCAL_PKG_LIST += gcc-host
+LOCAL_BASE_PKG_LIST := bzip2 cairo curl fontconfig freetype gcc icu4c jpeg libpng ncurses openssl pcre2 pixman readline tiff xz zlib librtmp zstd
+LOCAL_BASE_PKG_LIST += gcc-host
 
 #  --- libraries for other contributed R packages, development tools ---
 
-LOCAL_PKG_LIST += binutils boost cfitsio cmake expat ffmpeg fftw gdal geos gettext giflib glpk gmp gpgme gsl harfbuzz hdf4 hdf5 isl jasper jsoncpp lame lapack libarchive libassuan libffi libgeotiff libgit2 libiconv libsndfile libssh libssh2 libtool libuv libvpx libwebp libxml2 lz4 mpc mpfr nasm netcdf nettle nlopt openblas opencv pcre poppler proj protobuf tcl termcap x264 xvidcore yasm intel-tbb json-c
-LOCAL_PKG_LIST += freexl gpgme ogg spatialite tre vorbis yaml-cpp jsoncpp lzo openjpeg pkgconf sqlite libgit2
-LOCAL_PKG_LIST += imagemagick librsvg libmysqlclient sox gtk2 libzmq
+LOCAL_FULL_PKG_LIST += binutils boost cfitsio cmake expat ffmpeg fftw gdal geos gettext giflib glpk gmp gpgme gsl harfbuzz hdf4 hdf5 isl jasper jsoncpp lame lapack libarchive libassuan libffi libgeotiff libgit2 libiconv libsndfile libssh libssh2 libtool libuv libvpx libwebp libxml2 lz4 mpc mpfr nasm netcdf nettle nlopt openblas opencv pcre poppler proj protobuf tcl termcap x264 xvidcore yasm intel-tbb json-c
+LOCAL_FULL_PKG_LIST += freexl gpgme ogg spatialite tre vorbis yaml-cpp jsoncpp lzo openjpeg pkgconf sqlite libgit2
+LOCAL_FULL_PKG_LIST += imagemagick librsvg libmysqlclient sox gtk2 libzmq
 
 #  --- libraries for other contributed R packages, development tools, added for R ---
-LOCAL_PKG_LIST += msmpi udunits redland coinor-symphony libsbml jq libv8 libmariadbclient hiredis
+LOCAL_FULL_PKG_LIST += msmpi udunits redland coinor-symphony libsbml jq libv8 libmariadbclient hiredis
 
+ifeq ($(R_TOOLCHAIN_TYPE),base)
+    $(info Building base R toolchain)
+    LOCAL_PKG_LIST := $(LOCAL_BASE_PKG_LIST)
+else
+    $(info Building full R toolchain)
+    LOCAL_PKG_LIST := $(LOCAL_BASE_PKG_LIST) $(LOCAL_FULL_PKG_LIST)
+endif
 
 .DEFAULT_GOAL  := local-pkg-list
 local-pkg-list: $(LOCAL_PKG_LIST)
