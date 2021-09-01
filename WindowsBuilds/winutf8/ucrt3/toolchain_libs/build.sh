6110dccc85ba2bd3cee507b50424b9c479daa20b
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-09-01 09:07:34 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4718 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/build.sh
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/build.sh
@@ -15,21 +15,16 @@ cp make.out ../build
 
 cd usr
 
-# This includes a lot of .exe files, including tests, which are large and
-# probably not used by most users,
+# zstd has a slightly better compression ratio on these files, but not
+# substantialy for justifying a change in file names, etc
+#   tried with "zstd -T0 -22 --ultra > ../../build/gcc10_ucrt3.tar.zst"
 #
-# tar cfh - x86_64-w64-mingw32.static.posix | xz -T${CPUS} -9ve > ../../build/gcc10_ucrt3.txz
-
-find x86_64-w64-mingw32.static.posix -printf "%k %p\n" | sort -n | cut -d' ' -f2 | \
-  tar --exclude="*-tests" --exclude="test*.exe"     --exclude="*gdal*.exe" \
-    --exclude="*rtmp*.exe" --exclude="*gnutls*.exe" --exclude="hb-*.exe" \
-    --exclude="ogr*.exe" --exclude="certtool.exe" --exclude="gnmmanage.exe" \
-    --exclude="nearblack.exe" \
-    --create --dereference --no-recursion --files-from - --file - | \
-  zstd -T0 -22 --ultra > ../../build/gcc10_ucrt3.tar.zst
-
-# zstd has a slightly better compression ratio on these files, but keep xz
-# at least for now as not to break existing scripts
+# with both zstd and xz it significantly helps to archive files in the order
+# of file size, so that duplicated executables and executables linked to the
+# same libraries are more likely close in the archive
+#
+# excluding executables, particularly tests, which are not needed by most users,
+# but they take a lot of space because of static linking
 
 find x86_64-w64-mingw32.static.posix -printf "%k %p\n" | sort -n | cut -d' ' -f2 | \
   tar --exclude="*-tests" --exclude="test*.exe"     --exclude="*gdal*.exe" \
