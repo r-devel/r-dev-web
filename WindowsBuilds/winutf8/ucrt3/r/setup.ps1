60defc99911198720f0887efde0ad3827011a9bd
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-09-23 13:27:59 +0000

    Use per-user installation of MiKTeX 21.6. Ensure sequential invocation of programs during provisioning.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4761 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/r/setup.ps1
+++ WindowsBuilds/winutf8/ucrt3/r/setup.ps1
@@ -24,42 +24,78 @@ if (-not(Test-Path("C:\Program Files (x86)\InnoSetup"))) {
   } elseif (-not(Test-Path("innosetup.exe"))) {
     Invoke-WebRequest -Uri https://jrsoftware.org/download.php/is.exe?site=2 -OutFile innosetup.exe -UseBasicParsing
   }
-  .\innosetup.exe /VERYSILENT /ALLUSERS /NOICONS /DIR="C:\Program Files (x86)\InnoSetup"
+  Start-Process -Wait -FilePath ".\innosetup.exe" -ArgumentList "/VERYSILENT /ALLUSERS /NOICONS /DIR=`"C:\Program Files (x86)\InnoSetup`""
   cd ..
 }
 
 # Install MikTeX
+#
+# This uses MiKTeX basic installer, version 21.6 (which is not the latest as
+# of this writing, but 21.8 is crashing on "texify --version".  Installs it
+# per-user only avoid problems with per-user and system-wide installations
+# going out of sync.  Updated the package database, but does not upgrade the
+# packages, in order not to install the crashing version.
 
-# https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/miktexsetup-4.1-x64.zip
+# https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-21.6-x64.exe
 
-if (-not(Test-Path("C:\Program Files\MiKTeX\miktex\bin\x64"))) {
+if (-not(Test-Path("$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\"))) {
   cd temp
-  if (Test-Path("..\installers\miktexsetup-4.1-x64.zip")) {
-    cp "..\installers\miktexsetup-4.1-x64.zip" miktexsetup.zip
-  } elseif (-not(Test-Path("miktexsetup.zip"))) {
-    Invoke-WebRequest -Uri https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/miktexsetup-4.1-x64.zip -OutFile miktexsetup.zip -UseBasicParsing
-  }
-  Expand-Archive -DestinationPath . -Path miktexsetup.zip -Force
-
-  if (Test-Path("..\installers\miktex_repo")) {
-    $MREPO=Convert-Path "..\installers\miktex_repo"
-  } else {
-    if (-not(Test-Path("tmprepo"))) {
-      mkdir tmprepo
-    }
-    $MREPO=Convert-Path tmprepo
-    .\miktexsetup_standalone.exe --verbose --package-set=basic --shared --local-package-repository=$MREPO download
+  if (Test-Path("..\installers\basic-miktex-21.6-x64.exe")) {
+    cp "..\installers\basic-miktex-21.6-x64.exe" basic-miktex.exe
+  } elseif (-not(Test-Path("basic-miktex.exe"))) {
+    Invoke-WebRequest -Uri https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-21.6-x64.exe -OutFile basic-miktex.exe -UseBasicParsing
   }
 
-  .\miktexsetup_standalone.exe --verbose --package-set=basic --shared --local-package-repository=$MREPO install
-
-  & "C:\Program Files\MiKTeX\miktex\bin\x64\initexmf.exe" --admin --enable-installer
-  & "C:\Program Files\MiKTeX\miktex\bin\x64\initexmf.exe" --admin --set-config-value=[MPM]AutoInstall=t
-  & "C:\Program Files\MiKTeX\miktex\bin\x64\mpm.exe" --admin --verbose --update-db
-  & "C:\Program Files\MiKTeX\miktex\bin\x64\mpm.exe" --admin --verbose --update
-  & "C:\Program Files\MiKTeX\miktex\bin\x64\mpm.exe" --admin --install=inconsolata
+  Start-Process -Wait -FilePath ".\basic-miktex.exe" `
+    -ArgumentList "--unattended --package-set=basic --private --user-config=`"$env:APPDATA\MiKTeX`" --user-data=`"$env:LOCALAPPDATA\MiKTeX`" --user-install=`"$env:LOCALAPPDATA\Programs\MiKTeX`""
+  
+  Start-Process -Wait -FilePath "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\initexmf.exe" -ArgumentList "--enable-installer"
+  Start-Process -Wait -FilePath "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\initexmf.exe" -ArgumentList "--set-config-value=[MPM]AutoInstall=t"
+  Start-Process -Wait -FilePath "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\mpm.exe" -ArgumentList "--verbose --update-db"
+  Start-Process -Wait -FilePath "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\mpm.exe" -ArgumentList "--install=inconsolata"
   cd ..
 }
+
+# Install MikTeX
+#
+# This uses the MiKTeX setup utility and allows to have the downloaded set
+# of packages prepared for the virtual machine, to save traffic e.g.  with
+# repeated use.  However, it installs the latest version, 21.8 at the time
+# of this writing, which comes with texify crashing (texify --version). 
+# Also it is rather difficult to keep the system-wide and per-user package
+# sets in sync.
+#
+# https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/miktexsetup-4.1-x64.zip
+#
+#
+# if (-not(Test-Path("C:\Program Files\MiKTeX\miktex\bin\x64"))) {
+#  cd temp
+#  if (Test-Path("..\installers\miktexsetup-4.1-x64.zip")) {
+#    cp "..\installers\miktexsetup-4.1-x64.zip" miktexsetup.zip
+#  } elseif (-not(Test-Path("miktexsetup.zip"))) {
+#    Invoke-WebRequest -Uri https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/miktexsetup-4.1-x64.zip -OutFile miktexsetup.zip -UseBasicParsing
+#  }
+#  Expand-Archive -DestinationPath . -Path miktexsetup.zip -Force
+#
+#  if (Test-Path("..\installers\miktex_repo")) {
+#    $MREPO=Convert-Path "..\installers\miktex_repo"
+#  } else {
+#    if (-not(Test-Path("tmprepo"))) {
+#      mkdir tmprepo
+#    }
+#    $MREPO=Convert-Path tmprepo
+#    .\miktexsetup_standalone.exe --verbose --package-set=basic --shared --local-package-repository=$MREPO download
+#  }
+#
+#  .\miktexsetup_standalone.exe --verbose --package-set=basic --shared --local-package-repository=$MREPO install
+#
+#  & "C:\Program Files\MiKTeX\miktex\bin\x64\initexmf.exe" --admin --enable-installer
+#  & "C:\Program Files\MiKTeX\miktex\bin\x64\initexmf.exe" --admin --set-config-value=[MPM]AutoInstall=t
+#  & "C:\Program Files\MiKTeX\miktex\bin\x64\mpm.exe" --admin --verbose --update-db
+#  & "C:\Program Files\MiKTeX\miktex\bin\x64\mpm.exe" --admin --verbose --update
+#  & "C:\Program Files\MiKTeX\miktex\bin\x64\mpm.exe" --admin --install=inconsolata
+#  cd ..
+# }
 
 # Install Msys2
 
@@ -74,16 +110,16 @@ if (-not(Test-Path("C:\msys64"))) {
   }
   cd ..
 
-  .\temp\msys2-base.exe
+  Start-Process -Wait -FilePath ".\temp\msys2-base.exe"
 
   # from https://www.msys2.org/docs/ci/
     # Run for the first time
-  C:\msys64\usr\bin\bash -lc ' '
+  Start-Process -Wait -FilePath "C:\msys64\usr\bin\bash" -ArgumentList "-lc ' '"
 
     # Update MSYS2
-  C:\msys64\usr\bin\bash -lc 'pacman --noconfirm -Syuu'  # Core update (in case any core packages are outdated)
-  C:\msys64\usr\bin\bash -lc 'pacman --noconfirm -Syuu'  # Normal update
+  Start-Process -Wait -FilePath "C:\msys64\usr\bin\bash" -ArgumentList "-lc 'pacman --noconfirm -Syuu'"  # Core update (in case any core packages are outdated)
+  Start-Process -Wait -FilePath "C:\msys64\usr\bin\bash" -ArgumentList "-lc 'pacman --noconfirm -Syuu'"  # Normal update
 
     # Install Msys2 packages needed for building R
-  C:\msys64\usr\bin\bash -lc 'pacman --noconfirm -y -S unzip diffutils make winpty rsync texinfo tar texinfo-tex zip subversion bison moreutils xz patch'
+  Start-Process -Wait -FilePath "C:\msys64\usr\bin\bash" -ArgumentList "-lc 'pacman --noconfirm -y -S unzip diffutils make winpty rsync texinfo tar texinfo-tex zip subversion bison moreutils xz patch'"
 }
--- WindowsBuilds/winutf8/ucrt3/vm.md
+++ WindowsBuilds/winutf8/ucrt3/vm.md
@@ -297,7 +297,7 @@ tar xf gcc10*full*.tar.zst
 
 export R_CUSTOM_TOOLS_SOFT=`pwd`/x86_64-w64-mingw32.static.posix
 export PATH=$R_CUSTOM_TOOLS_SOFT/bin:$PATH
-export PATH=/c/Program\ Files/MiKTeX/miktex/bin/x64:$PATH
+export PATH=~/AppData/Local/Programs/MiKTeX/miktex/bin/x64:$PATH
 export TAR="/usr/bin/tar --force-local"
 ```
 
