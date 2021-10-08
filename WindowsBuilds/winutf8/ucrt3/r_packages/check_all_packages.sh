1c8865676df77b6165f9e84d70bfae6a47d4211e
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2021-10-08 16:10:48 +0000

    Support for checking packages in docker.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@4785 c52295ea-58df-0310-926a-d16021944841

---
+++ WindowsBuilds/winutf8/ucrt3/r_packages/check_all_packages.sh
@@ -0,0 +1,183 @@
+#! /bin/bash
+
+# Checks R packages using R installed to "rinst" with installation-time
+# patches install to "patches"/"patches_idx.rds", using toolchain installed
+# to "x86_64-w64-mingw32.static.posix", using repository of binary packages
+# in "build". This script also installs the packages, first.
+#
+# "pkgcheck" will be produced and the results will be under "pkgcheck/results".
+#
+# MiKTeX must be in /c/Program Files/MiKTeX/miktex/bin/x64 or in
+# $LOCALAPPDATA/Programs/MiKTeX/miktex/bin/x64 or on PATH.
+#
+# JDK must be in /c/Program\ Files/AdoptOpenJDK/jdk-11.0.11.9-hotspot or
+# on PATH
+#
+# JAGS must be in /c/Program\ Files/JAGS/JAGS-4.3.0 or under JAGS_ROOT
+#
+# Pandoc must be in /c/Program\ Files/Pandoc or on PATH
+#
+# Ghostscript must be in /c/Program\ Files/gs/gs/bin or on PATH
+#
+# HANDLE_TOOL must be set or handle.exe be
+#   "/c/Program Files/sysinternals/handle64.exe"
+#
+# TLIST_TOOL must be set or tlist.exe be
+#   "/c/Program Files (x86)/Windows Kits/10/Debuggers/x64/tlist.exe"
+#
+# FIXME: currently a lot of duplication with build_packages.sh
+
+
+# MiKTeX 
+
+MIKDIR="/c/Program Files/MiKTeX/miktex/bin/x64"
+if [ ! -x "${MIKDIR}/pdflatex" ] ; then
+  MIKDIR=`cygpath "$LOCALAPPDATA/Programs/MiKTeX/miktex/bin/x64"`
+  if [ ! -x "${MIKDIR}/pdflatex" ] ; then
+    WPDFLATEX=`which pdflatex 2>/dev/null`
+    if [ "X${WPDFLATEX}" != X ] ; then
+      MIKDIR=`dirname "${WPDFLATEX}"`
+    fi
+  fi
+fi
+
+if [ ! -x "${MIKDIR}/pdflatex" ] ; then
+  echo "MikTeX is not available." >&2
+  exit 1
+fi
+
+export PATH=$MIKDIR:$PATH
+
+# update miktex (otherwise pdflatex mail complain and building
+# manuals/vignettes may fail)
+
+mpm --update-db --verbose
+
+# Java
+
+JDIR=/c/Program\ Files/AdoptOpenJDK/jdk-11.0.11.9-hotspot
+if [ ! -x "${JDIR}/java" ] ; then
+  WJAVA=`which java 2>/dev/null`
+  if [ "X${WJAVA}" != X ] ; then
+    JDIR=`dirname ${WJAVA}`
+    JDIR=`( cd "${JDIR}"/.. ; pwd )`
+  fi
+fi
+
+if [ ! -x "${JDIR}/bin/java" ] ; then
+  echo "Java is not available." >&2
+  exit 1
+fi
+
+if [ "X${JAVA_HOME}" == X ] ; then
+  export JAVA_HOME="${JDIR}"
+fi
+
+export PATH="${JDIR}/bin:$PATH"
+
+# JAGS
+
+JROOT=/c/Program\ Files/JAGS/JAGS-4.3.0
+if [ ! -x "${JROOT}/x64/bin/jags-terminal.exe" ] ; then
+  JROOT="${JAGS_ROOT}"
+fi
+
+if [ ! -x "${JROOT}/x64/bin/jags-terminal.exe" ] ; then
+  echo "JAGS is not available." >&2
+  exit 1
+fi
+
+export JAGS_ROOT="${JROOT}"
+
+# Pandoc
+
+PDIR=/c/Program\ Files/Pandoc
+if [ ! -x "${PDIR}/pandoc" ] ; then
+  WPANDOC=`which pandoc 2>/dev/null`
+  if [ "X${WPANDOC}" != X ] ; then
+    PDIR=`dirname "${WPANDOC}"`
+  fi
+fi
+
+if [ ! -x "${PDIR}/pandoc" ] ; then
+  echo "Pandoc is not available." >&2
+  exit 1
+fi
+
+export PATH="${PDIR}:$PATH"
+
+# Ghostscript
+
+GDIR=/c/Program\ Files/gs/gs/bin
+if [ ! -x "${GDIR}/gswin64" ] ; then
+  WGS=`which gswin64 2>/dev/null`
+  if [ "X${WGS}" != X ] ; then
+    GDIR=`dirname "${WGS}"`
+  fi
+fi
+
+if [ ! -x "${GDIR}/gswin64" ] ; then
+  echo "Ghostscript is not available." >&2
+  exit 1
+fi
+
+# Handle
+
+if [ "X${HANDLE_TOOL}" == X ] ; then
+  export HANDLE_TOOL="/c/Program Files/sysinternals/handle64.exe"
+fi
+
+if [ ! -x "${HANDLE_TOOL}" ] ; then
+  echo "HANDLE_TOOL (handle.exe) is not available." >&2
+  exit 1
+fi
+
+# Tlist
+
+if [ "X${TLIST_TOOL}" == X ] ; then
+  export TLIST_TOOL="/c/Program Files (x86)/Windows Kits/10/Debuggers/x64/tlist.exe"
+fi
+
+if [ ! -x "${TLIST_TOOL}" ] ; then
+  echo "TLIST_TOOL (tlist.exe) is not available." >&2
+  exit 1
+fi
+
+
+
+# ----------- 
+
+export PATH=`pwd`/rinst/bin/:`pwd`/rinst/Tcl/bin/:$PATH
+export _R_INSTALL_TIME_PATCHES_=`pwd`
+export R_CUSTOM_TOOLS_SOFT=`pwd`/x86_64-w64-mingw32.static.posix
+  # intentionally non-existent directory as using Msys2 which is on PATH
+export R_CUSTOM_TOOLS_PATH=custom_rtools
+export PATH=$R_CUSTOM_TOOLS_SOFT/bin:$PATH
+
+export LC_CTYPE=
+export TAR="/usr/bin/tar --force-local"
+
+# -----------
+
+echo "=== Environment used for checking: " >&2
+set >&2
+
+echo "=== GCC Version: " >&2
+gcc -v >&2
+
+echo "=== Makeconf: " >&2
+cat rinst/etc/x64/Makeconf >&2
+
+echo " === R svn version: " >&2
+Rscript -e "R.version\$svn" >&2
+
+# install packages (re-uses some binary packages)
+
+echo "`date`: Installing packages for checking" >&2
+R --vanilla < install_packages_for_checking.r 2>&1 | \
+  tee install_packages_for_checking.out
+echo "`date`: Done installing packages for checking" >&2
+
+# check packages (in parallel) and generate reports
+
+bash -x ./check_packages.sh 2>&1 | tee check_packages.out
---
+++ WindowsBuilds/winutf8/ucrt3/r_packages/check_all_packages_in_docker.sh
@@ -0,0 +1,136 @@
+#! /bin/bash
+
+# Install CRAN packages and check them in a docker container.  Requires a
+# local installation of R (in "rinst", created as a per-user installation
+# using the R installer), a local copy of R package patches (in "patches")
+# and an index ("patches_idx.rds"), a local mirror of CRAN+BIOC source
+# packages in "mirror", repository of binary packages produced by
+# build_packages.sh in "build".
+#
+# tlist.exe (from Windows Debugging Tools, Windows SDK) must be specified by
+# TLIST_TOOL environment variable or be in "/c/Program Files (x86)/Windows
+# Kits/10/Debuggers/x64/tlist.exe" on the host machine (unfortunately this
+# tool cannot be downloaded and installed automatically with reasonable
+# effort, so it is copied into the container).
+# 
+# It produces "pkgcheck" and the results are in "pkgcheck/results".
+#
+# docker must be on PATH and the current user must be allowed to use it
+#
+# To inspect the container, do
+#    winpty docker exec -it checkrpkgs PowerShell
+#
+# FIXME: the container setup is the same as for building packages
+
+DOCKER=`which docker`
+if [ "X$DOCKER" == X ]; then
+  echo "Docker not on PATH."
+  exit 1
+fi
+
+if [ -d "pkgcheck" ] ; then
+  echo "Directory pkgcheck already exists."
+  exit 1
+fi
+
+if [ "X${TLIST_TOOL}" == X ] ; then
+  TLIST_TOOL="/c/Program Files (x86)/Windows Kits/10/Debuggers/x64/tlist.exe"
+fi
+if [ ! -r "${TLIST_TOOL}" ] ; then
+  echo "TLIST_TOOL (tlist.exe) not available."
+  exit 1
+fi
+
+CID=checkrpkgs
+X=`docker container ls -a | sed -e 's/.* //g' | grep -v NAMES | grep $CID`
+
+# Windows 10 with Hyper-V requires stopped containers
+# for file-system operations
+
+mkdir -p build
+
+if [ "X$X" != X$CID ] ; then
+  echo "Creating container $CID"
+  docker create --name $CID -it \
+    -v `cygpath -w $(pwd)`:'c:\r_packages_ro':ro \
+    --storage-opt size=300G \
+    mcr.microsoft.com/windows/server:ltsc2022
+  
+  if [ -d installers ] ; then
+    docker cp installers $CID:\\
+  fi
+
+  docker cp ../r/setup.ps1 $CID:\\
+  docker cp setup_checks.ps1 $CID:\\  
+
+# install MiKTeX using the standalone installer
+#  docker cp setup_miktex_standalone.ps1 $CID:\\
+
+  docker start $CID
+  docker exec $CID PowerShell -File setup.ps1 >checkrpkgs_setup1.out 2>&1
+
+# install MiKTeX using the standalone installer
+#  docker exec $CID PowerShell -File setup_miktex_standalone.ps1 >build/checkrpkgs_setup_miktex_standalone.out 2>&1
+
+  docker exec $CID PowerShell -File setup_checks.ps1 >checkrpkgs_setup2.out 2>&1
+
+  docker exec $CID PowerShell -c mkdir r_packages
+  
+  # set/enable short path names for "Program Files" (needed e.g. by rJava) to
+  # get a name without spaces, needed at least in "server:ltsc2022"
+  
+  docker exec $CID cmd //c fsutil file setshortname "Program Files" PROGRA~1
+  docker exec $CID cmd //c fsutil file setshortname "Program Files (x86)" PROGRA~2
+  
+  # install tlist
+  
+  docker exec $CID PowerShell -c '
+    New-Item -Path "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\" -ItemType Directory -ErrorAction SilentlyContinue
+  '
+  docker stop $CID
+  docker cp "${TLIST_TOOL}" "$CID:\\Program Files (x86)\\Windows Kits\\10\\Debuggers\\x64\\"
+  docker start $CID
+  
+else
+  echo "Reusing container $CID"
+  # reuse existing container
+  
+  docker start $CID
+  # Remove-Item cannot delete symlinks 
+  # docker exec $CID PowerShell -c Remove-Item -Path r_packages -Recurse -Force
+  docker exec $CID cmd //c rmdir //s //q r_packages
+  docker exec $CID PowerShell -c mkdir r_packages
+fi    
+
+docker exec $CID PowerShell -c '
+  cd r_packages
+  New-Item -Path "mirror" -ItemType SymbolicLink -Value "C:\r_packages_ro\mirror"
+  New-Item -Path "x86_64-w64-mingw32.static.posix" -ItemType SymbolicLink -Value "C:\r_packages_ro\x86_64-w64-mingw32.static.posix"
+  New-Item -Path "build" -ItemType SymbolicLink -Value "C:\r_packages_ro\build"
+  New-Item -Path "rinst" -ItemType SymbolicLink -Value "C:\r_packages_ro\rinst"
+  New-Item -Path "patches" -ItemType SymbolicLink -Value "C:\r_packages_ro\patches"
+  New-Item -Path "patches_idx.rds" -ItemType SymbolicLink -Value "C:\r_packages_ro\patches_idx.rds"
+  New-Item -Path "check_all_packages.sh" -ItemType SymbolicLink -Value "C:\r_packages_ro\check_all_packages.sh"
+  New-Item -Path "install_packages_for_checking.r" -ItemType SymbolicLink -Value "C:\r_packages_ro\install_packages_for_checking.r"
+  New-Item -Path "check_packages.sh" -ItemType SymbolicLink -Value "C:\r_packages_ro\check_packages.sh" 
+'
+
+docker exec $CID PowerShell -c \
+  'cd r_packages ; $env:CHERE_INVOKING="yes" ; $env:MSYSTEM="MSYS" ; C:\msys64\usr\bin\bash -lc ./check_all_packages.sh'\' $*\' 2>&1 | tee checkrpkgs_build.out
+  
+docker stop $CID
+
+mkdir pkgcheck
+
+docker cp $CID:\\r_packages\\pkgcheck\\install.out pkgcheck
+docker cp $CID:\\r_packages\\pkgcheck\\results pkgcheck
+
+# not needed normally
+#   docker cp $CID:\\r_packages\\pkgcheck\\lib .
+#   docker cp $CID:\\r_packages\\pkgcheck\\CRAN .
+
+docker cp $CID:\\r_packages\\install_packages_for_checking.out .
+docker cp $CID:\\r_packages\\check_packages.out .
+
+# not deleting the container so that it can be re-used
+# docker rm -f $CID
