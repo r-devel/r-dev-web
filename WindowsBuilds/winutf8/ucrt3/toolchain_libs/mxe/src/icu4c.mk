56c0fcf086a3538c2ed8558a5f26e2300043d6cd
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2022-01-19 19:45:54 +0000

    Upgrade ICU.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5000 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/icu4c.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/icu4c.mk
@@ -1,12 +1,13 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
+# test
 
 PKG             := icu4c
 $(PKG)_WEBSITE  := https://github.com/unicode-org/icu
 $(PKG)_DESCR    := ICU4C
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 66.1
+$(PKG)_VERSION  := 70.1
 $(PKG)_MAJOR    := $(word 1,$(subst ., ,$($(PKG)_VERSION)))
-$(PKG)_CHECKSUM := 52a3f2209ab95559c1cf0a14f24338001f389615bf00e2585ef3dbc43ecf0a2e
+$(PKG)_CHECKSUM := 8d205428c17bf13bb535300669ed28b338a157b1c01ae66d31d0d3e2d47c3fd5
 $(PKG)_GH_CONF  := unicode-org/icu/releases/latest,release-,,,-
 $(PKG)_SUBDIR   := icu
 $(PKG)_URL      := $($(PKG)_WEBSITE)/releases/download/release-$(subst .,-,$($(PKG)_VERSION))/icu4c-$(subst .,_,$($(PKG)_VERSION))-src.tgz
