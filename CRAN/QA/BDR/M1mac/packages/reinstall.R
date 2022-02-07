8e98682e7999d81b767c092c06e403fae139c949
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2022-02-07 20:23:39 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5033 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/M1mac/packages/reinstall.R
+++ CRAN/QA/BDR/M1mac/packages/reinstall.R
@@ -13,7 +13,6 @@ foo <- setdiff(foo, noupdate)
 setRepositories(ind = 1:4)
 options(repos = c(getOption('repos'),
 #                  INLA = 'https://inla.r-inla-download.org/R/stable/',
-                  webshsot2 = "https://dmurdoch.github.io/drat")
-#                  Omegahat = "http://www.omegahat.net/R"))
+                  webshsot2 = "https://dmurdoch.github.io/drat"))
 
 install.packages(foo, Ncpus = 8, type="source", INSTALL_opts = opts)
