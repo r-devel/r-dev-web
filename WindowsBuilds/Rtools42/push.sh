366bacee6edcb6748f7b1d32efa52cb3c4601c60
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2022-03-17 12:23:35 +0000

    Initial version.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5089 c52295ea-58df-0310-926a-d16021944841

---
+++ WindowsBuilds/Rtools42/push.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+
+SRC=~/winutf8/ucrt3
+TGT=~/rtools42
+TMPTGT=~/rtools42.tmp
+
+# copy toolchain files
+
+cd $SRC
+RTEXE=`ls -1 rtools42-*.exe`
+if [ "X$RTEXE" == X ] ; then
+  echo "New Rtools42 installer not ready." >&2
+  exit 1
+fi
+
+if [ -r $TGT/files/$RTEXE ] ; then
+  echo "Already have the latest version." >&2
+  exit 0
+fi
+
+TLVER=`echo $RTEXE | cut -d- -f2`
+
+TBASE=gcc10_ucrt3_base_$TLVER.tar.zst
+TFULL=gcc10_ucrt3_full_$TLVER.tar.zst
+TCROSS=gcc10_ucrt3_cross_$TLVER.tar.zst
+
+rm -rf $TMPTGT
+mkdir -p $TMPTGT
+
+for F in $RTEXE $TBASE $TFULL $TCROSS ; do
+  if [ ! -r $F ] ; then
+    echo "File $F is missing." >&2
+    exit 1
+  fi
+  cp -p $F $TMPTGT
+done
+
+for F in $RTEXE $TBASE $TFULL $TCROSS ; do
+  if ! cmp $F $TMPTGT/$F ; then
+    echo "Files changed while copying." >&2
+    exit 2
+  fi
+done
+
+# rename as needed
+
+cd $TMPTGT
+mkdir files
+
+NTBASE=rtools42-toolchain-libs-base-$TLVER.tar.zst
+NTFULL=rtools42-toolchain-libs-full-$TLVER.tar.zst
+NTCROSS=rtools42-toolchain-libs-cross-$TLVER.tar.zst
+
+mv $RTEXE files
+mv $TBASE files/$NTBASE
+mv $TFULL files/$NTFULL
+mv $TCROSS files/$NTCROSS
+
+# generate index file
+
+svn cat https://svn.r-project.org/R-dev-web/trunk/WindowsBuilds/Rtools42/rtools.md | \
+  sed -e 's/RTVER/'$TLVER'/g' |
+  pandoc -s -c "https://cran.r-project.org/R.css" > rtools.html
+  
+if [ $? -ne 0 ] || [ ! -r rtools.html ] ; then
+  echo "Failed to generate index." >&2
+  exit 1
+fi
+
+rm -rf $TGT.old
+if [ -d $TGT ] ; then
+  mv $TGT $TGT.old
+fi
+mv $TMPTGT $TGT
