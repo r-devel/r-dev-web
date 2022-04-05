6e0dfdce7f211f78512f8460f7c2279a55a592d1
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2022-04-05 16:23:54 +0000

    Fix udunits download location.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5161 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/udunits.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/udunits.mk
@@ -6,7 +6,7 @@ $(PKG)_VERSION  := 2.2.28
 $(PKG)_CHECKSUM := 590baec83161a3fd62c00efa66f6113cec8a7c461e3f61a5182167e0cc5d579e
 $(PKG)_SUBDIR   := udunits-$($(PKG)_VERSION)
 $(PKG)_FILE     := udunits-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://artifacts.unidata.ucar.edu/repository/downloads-$(PKG)/$($(PKG)_FILE)
+$(PKG)_URL      := https://artifacts.unidata.ucar.edu/repository/downloads-$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc expat
 
 define $(PKG)_BUILD
