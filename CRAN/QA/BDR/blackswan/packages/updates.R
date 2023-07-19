fe7fe3036fd5ce2d9d1abddbb29c325e709ad7f8
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2023-07-19 09:44:02 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5700 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/blackswan/packages/updates.R
+++ CRAN/QA/BDR/blackswan/packages/updates.R
@@ -12,7 +12,7 @@ Sys.setenv(DISPLAY = ':5',
            RMPI_LIB_PATH = "/usr/lib64/openmpi/lib",
  	   R_MAX_NUM_DLLS = "150")
 
-noupdate <- c()
+noupdate <- c("cpp11")
 
 chooseBioCmirror(ind=1)
 setRepositories(ind = c(1:4))
