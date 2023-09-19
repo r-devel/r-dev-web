293317a4809278d1c658df203da8559573f1542c
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2023-09-19 12:33:17 +0000

    Upgrade llvm.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5782 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/plugins/llvm-mingw/llvm.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/plugins/llvm-mingw/llvm.mk
@@ -7,8 +7,8 @@ $(PKG)_WEBSITE  := https://llvm.org/
 $(PKG)_DESCR    := A collection of modular and reusable compiler and toolchain technologies
 $(PKG)_IGNORE   :=
 # This version needs to be in-sync with the compiler-rt-sanitizers package
-$(PKG)_VERSION  := 16.0.5
-$(PKG)_CHECKSUM := 37f540124b9cfd4680666e649f557077f9937c9178489cea285a672e714b2863
+$(PKG)_VERSION  := 16.0.6
+$(PKG)_CHECKSUM := ce5e71081d17ce9e86d7cbcfa28c4b04b9300f8fb7e78422b1feb6bc52c3028e
 $(PKG)_PATCHES  := $(realpath $(sort $(wildcard $(dir $(lastword $(MAKEFILE_LIST)))/llvm-[0-9]*.patch)))
 $(PKG)_GH_CONF  := llvm/llvm-project/releases/latest,llvmorg-,,,,.tar.xz
 $(PKG)_SUBDIR   := $(PKG)-project-$(subst -,,$($(PKG)_VERSION)).src
