a31e297488cced9b75c6593423f0d251b041034f
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2023-10-04 08:48:25 +0000

    Update hdf5 to 1.12.1
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5820 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/hdf5.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/hdf5.mk
@@ -4,8 +4,8 @@ PKG             := hdf5
 $(PKG)_WEBSITE  := https://www.hdfgroup.org/hdf5/
 $(PKG)_DESCR    := HDF5
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.12.0
-$(PKG)_CHECKSUM := 97906268640a6e9ce0cde703d5a71c9ac3092eded729591279bf2e3ca9765f61
+$(PKG)_VERSION  := 1.12.1
+$(PKG)_CHECKSUM := aaf9f532b3eda83d3d3adc9f8b40a9b763152218fa45349c3bc77502ca1f8f1c
 $(PKG)_SUBDIR   := hdf5-$($(PKG)_VERSION)
 $(PKG)_FILE     := hdf5-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-$(call SHORT_PKG_VERSION,$(PKG))/hdf5-$($(PKG)_VERSION)/src/$($(PKG)_FILE)
@@ -22,13 +22,13 @@ define $(PKG)_BUILD
     mkdir '$(1)/pregen/shared'
     $(if $(findstring x86_64, $(TARGET)), \
       cp '$(1)/src/H5Tinit.c.mingw64' '$(1)/pregen/H5Tinit.c'
-      cp '$(1)/src/H5lib_settings.c.mingw64' '$(1)/pregen/H5lib_settings.c'
       cp '$(1)/src/H5Tinit.c.mingw64' '$(1)/pregen/shared/H5Tinit.c'
+      cp '$(1)/src/H5lib_settings.c.mingw64' '$(1)/pregen/H5lib_settings.c'
       cp '$(1)/src/H5lib_settings.c.mingw64' '$(1)/pregen/shared/H5lib_settings.c',
       $(if $(findstring i686, $(TARGET)), \
         cp '$(1)/src/H5Tinit.c.mingw32' '$(1)/pregen/H5Tinit.c'
-        cp '$(1)/src/H5lib_settings.c.mingw32' '$(1)/pregen/H5lib_settings.c'
         cp '$(1)/src/H5Tinit.c.mingw32' '$(1)/pregen/shared/H5Tinit.c'
+        cp '$(1)/src/H5lib_settings.c.mingw32' '$(1)/pregen/H5lib_settings.c'
         cp '$(1)/src/H5lib_settings.c.mingw32' '$(1)/pregen/shared/H5lib_settings.c',
         $(error "Unexpected Target $(TARGET)")
       )
@@ -51,7 +51,7 @@ define $(PKG)_BUILD
             -DH5_DISABLE_SOME_LDOUBLE_CONV_RUN__TRYRUN_OUTPUT="" \
             -DH5_NO_ALIGNMENT_RESTRICTIONS_RUN=0 \
             -DH5_NO_ALIGNMENT_RESTRICTIONS_RUN__TRYRUN_OUTPUT="" \
-            -DH5_PRINTF_LL_TEST_RUN=1 \
+            -DH5_PRINTF_LL_TEST_RUN=0 \
             -DH5_PRINTF_LL_TEST_RUN__TRYRUN_OUTPUT="" \
             -DTEST_LFS_WORKS_RUN=0 \
             -DBUILD_TESTING=OFF \
@@ -61,21 +61,16 @@ define $(PKG)_BUILD
             -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON \
             -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON \
             -DONLY_SHARED_LIBS:BOOL=$(if $(BUILD_SHARED),ON,OFF) \
+            -DBUILD_SHARED=$(CMAKE_SHARED_BOOL) \
+            -DBUILD_STATIC=$(CMAKE_STATIC_BOOL) \
             -DHDF5_BUILD_TOOLS:BOOL=OFF \
+            -DHDF5_BUILD_DOC=OFF \
+            -DHDF5_BUILD_HL_LIB=ON \
+            -DHDF5_BUILD_CPP_LIB=ON \
         '$(1)'
 
     $(MAKE) -C '$(1)/.build' -j '$(JOBS)'
     $(MAKE) -C '$(1)/.build' -j 1 install
-
-    # Remove version suffix from pkg-config files
-    mv '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5-$($(PKG)_VERSION).pc' \
-       '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5.pc'
-    mv '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_hl-$($(PKG)_VERSION).pc' \
-       '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_hl.pc'
-    mv '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_cpp-$($(PKG)_VERSION).pc' \
-       '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_cpp.pc'
-    mv '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_hl_cpp-$($(PKG)_VERSION).pc' \
-       '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_hl_cpp.pc'
 
     # by error there is -lfull_path_to_libz.a
     $(SED) -i -e 's!-l[^ ]*libz\(.dll\)\?\.a!-lz!g' '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5.pc'
@@ -84,6 +79,13 @@ define $(PKG)_BUILD
     # by error, -lhdf5 is last, move it to the front of the list
     $(SED) -i -e 's!Libs.private:\(.*\)-lhdf5$$!Libs.private: -lhdf5\1!g' \
         '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5.pc'
+
+    # by error, pkg-config files are still referred to with version suffix
+    $(SED) -i -e 's!hdf5-'$($(PKG)_VERSION)'!hdf5!g' \
+              -e 's!hdf5_hl-'$($(PKG)_VERSION)'!hdf5_hl!g' \
+              '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_cpp.pc' \
+              '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_hl.pc' \
+              '$(PREFIX)/$(TARGET)/lib/pkgconfig/hdf5_hl_cpp.pc'
 
     # Test
     '$(TARGET)-gcc' \
