1ad4f6aa637ac3e90bad426b28996c1d944a825f
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2023-10-21 07:12:32 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5872 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/reinstall.R
+++ CRAN/QA/BDR/gannet/packages/reinstall.R
@@ -35,9 +35,9 @@ if(clang) {
     Sys.setenv(PKG_CONFIG_PATH = '/usr/local/clang/lib64/pkgconfig:/usr/local/lib64/pkgconfig:/usr/lib64/pkgconfig',
                JAGS_LIB = '/usr/local/clang/lib64',
                PATH=paste("/usr/local/clang/bin", Sys.getenv("PATH"), sep=":"))
-    ex <- c('V8', 'Rdisop', 'mzR')
+    ex <- c('V8', 'Rdisop', 'mzR', 'EBImage', 'Rsamtools')
     foo <- setdiff(foo, ex)
-}
+} else foo <- setdiff(foo, "beachmat")
 
 opts <- list(Rserve = "--without-server")
 
