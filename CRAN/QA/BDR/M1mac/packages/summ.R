59b9fa7d989bba0533c8db3ea1a47881f653bf8f
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2024-01-26 16:49:17 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5953 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/M1mac/packages/summ.R
+++ CRAN/QA/BDR/M1mac/packages/summ.R
@@ -1,8 +1,9 @@
 #! /opt/R/arm64/bin/Rscript
 files <- dir("~/R/packages/tests-devel", patt = "[.]out$", full.names = TRUE)
 known <- readLines("~/R/packages/M1known", warn = FALSE)
-known <- c(known, scan("~/R/packages/known", '', quiet = TRUE))
+known <- c(known, readLines("~/R/packages/known", warn = FALSE))
 known <- known[nzchar(known)]
+known <- grep("^#", known, invert = TRUE, value = TRUE)
 known <- paste0("/Users/ripley/R/packages/tests-devel/", known, ".out")
 files <- setdiff(files, known)
 for (f in known) {
