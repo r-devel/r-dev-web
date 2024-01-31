47fa2292b4bd26c67808f403340af5ad2cb20205
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2024-01-31 18:32:17 +0000

    Fix flang wrapper.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5957 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/plugins/llvm-mingw/host-toolchain/llvm-mingw-host.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/plugins/llvm-mingw/host-toolchain/llvm-mingw-host.mk
@@ -64,7 +64,7 @@ define $(PKG)_BUILD
         $(INSTALL) -m755 '$(WRAPPER).exe' '$(PREFIX)/$(TARGET)/bin';)
 
     # flang runtime also needs c++ library
-    $(SED) -i -e 's|exec_argv[arg] = NULL;| exec_argv[arg++] = _T("--start-no-unused-arguments"); exec_argv[arg++] = _T("-lc++"); exec_argv[arg++] = _T("--start-no-unused-arguments"); \0|' \
+    $(SED) -i -e 's|exec_argv\[arg\] = NULL;| exec_argv[arg++] = _T("--start-no-unused-arguments"); exec_argv[arg++] = _T("-lc++"); exec_argv[arg++] = _T("--end-no-unused-arguments"); \0|' \
                  '$(SOURCE_DIR)/wrappers/clang-target-wrapper.c'
 
     cd '$(BUILD_DIR)' && \
