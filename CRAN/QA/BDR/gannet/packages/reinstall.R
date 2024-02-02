b45e983572229d217ea6653b72048234b39cef02
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2024-02-02 05:08:50 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@5964 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/reinstall.R
+++ CRAN/QA/BDR/gannet/packages/reinstall.R
@@ -36,7 +36,7 @@ if(clang) {
     Sys.setenv(PKG_CONFIG_PATH = '/usr/local/clang/lib64/pkgconfig:/usr/local/lib64/pkgconfig:/usr/lib64/pkgconfig',
                JAGS_LIB = '/usr/local/clang/lib64',
                PATH=paste("/usr/local/clang/bin", Sys.getenv("PATH"), sep=":"))
-    ex <- c('V8', 'Rdisop', 'mzR', 'EBImage', 'Rsamtools')
+    ex <- c('V8', 'Rdisop', 'mzR', 'EBImage', 'Rsamtools', "x13binary")
     foo <- setdiff(foo, ex)
 }
 
