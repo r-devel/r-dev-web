f328972946aecbc2824d0cee6fc74300cf9a59fe
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2024-03-22 13:57:58 +0000

    Scripts for building aarch64 binaries on Linux.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6140 c52295ea-58df-0310-926a-d16021944841

---
+++ WindowsBuilds/winutf8/ucrt3/r/cross_build_binaries.sh
@@ -0,0 +1,98 @@
+#! /bin/bash
+
+set -e
+
+# cross_build_binaries.sh
+#
+# This cross-compiles the native code in R, based and recommended packages
+# and produces a tarball, which can be used on a Windows host to build and R
+# installer.
+#
+# The anticipated use is: run this on Linux/x86_64 machine to build binaries
+# for Windows/aarch64, then finish building the installer on Windows/x86_64
+# machine.  The installer is a Windows/x86_64 application but installs code
+# built for Windows/aarch64.
+#
+# The MXE-based 64-bit UCRT cross-toolchain must be in /usr/lib/mxe/usr_aarch64.
+#
+# The Tcl/Tk bundle must be present in the current directory (See ../tcl_bundle).
+#
+# This will produce a tarball in the current directory. It will include also the Tcl/Tk
+# bundle.
+
+TRIPLET=aarch64-w64-mingw32.static.posix
+TCLFILE=`ls -1 Tcl-aarch64*zip | head -1`
+TLDIR=/usr/lib/mxe/usr_aarch64
+
+CLEVEL=22 # though overkill
+EXTLIBS=$TLDIR/$TRIPLET
+
+if [ X"$TCLFILE" == X ] || [ ! -r $TCLFILE ] ; then
+  echo "Tcl/tk bundle file not found." >&2
+  exit 1
+fi
+
+if [ ! -d $EXTLIBS/lib ] || [ ! -x $TLDIR/bin/$TRIPLET-clang ] ; then
+  echo "Cross-toolchain not found." >&2
+  exit 1
+fi
+
+rm -rf cross
+mkdir cross
+cd cross
+
+# ---  build Linux (build) version
+
+svn checkout https://svn.r-project.org/R/trunk
+cd trunk
+./tools/rsync-recommended
+cd ..
+
+mkdir build
+cd build
+../trunk/configure  2>&1 | tee configure.out
+make -j
+cd ..
+
+# --- build Windows binaries (R, base packages)
+
+cd trunk
+unzip ../../$TCLFILE
+
+cd src/gnuwin32
+cat > MkRules.local <<EOF
+USE_LLVM = 1
+WIN =
+BINPREF64 = aarch64-w64-mingw32.static.posix-
+EXT_LIBS = $EXTLIBS
+EOF
+
+env PATH=$TLDIR/bin:$PATH make MkRules
+env PATH=$TLDIR/bin:$PATH make -j rbuild
+env PATH=$TLDIR/bin:$PATH make -j rpackages-cross
+env PATH=$TLDIR/bin:$PATH make -j cairodevices
+cd ../..
+cd ../
+
+# --- build Windows binaries for recommended packages
+
+# mkdir -p build/etc/
+echo "R_XTRA_CPPFLAGS = -I`pwd`/trunk/include -DNDEBUG" > build/etc/Makeconf
+cat trunk/etc/Makeconf >> build/etc/Makeconf
+
+# mkdir -p build/bin
+cp -p trunk/bin/*.dll build/bin
+
+PKGS1="lattice Matrix nlme MASS survival"
+PKGS2="boot class cluster codetools foreign KernSmooth mgcv nnet rpart spatial"
+
+for P in $PKGS1 $PKGS2 ; do
+  env PATH=$TLDIR/bin:$PATH R_CROSS_BUILD=singlearch R_LIBS=`pwd`/trunk/library \
+      ./build/bin/R CMD INSTALL --libs-only ./trunk/src/library/Recommended/${P}.tgz
+done
+
+# --- pack the results
+
+tar cf - trunk/bin trunk/modules trunk/library trunk/Tcl trunk/etc/Makeconf | \
+  zstd -T0 -$CLEVEL --ultra > r_binaries.tar.zst
+
---
+++ WindowsBuilds/winutf8/ucrt3/r/cross_build_binaries_in_docker.sh
@@ -0,0 +1,132 @@
+#! /bin/bash
+
+# Cross-compile binaries of the latest R-devel and recommended packages into
+# a bundle, which can be used on a Windows host to cross-build an R
+# installer.
+#
+# Currently this supports building Windows/aarch64 binaries on Linux/x86_64
+# host.
+#
+# cross_build_binaries_in_docker.sh
+#
+# The needed files are
+#
+#     llvm17_ucrt3_base_aarch64_REV.tar.zst
+#     llvm17_ucrt3_cross_aarch64_REV.tar.zst
+#     Tcl-aarch64-REV-REV1.zip 
+#
+# in the current directory: the base toolchain libraries, the
+# cross-toolchain (../toolchain_libs, and the Tcl bundle for the target
+# (../tcl_bundle).
+#
+# The results appear in current directory under "cross", including a tarball
+# of the result binaries and the build tree.  The build uses the latest
+# version from subversion, the revision number can be found in file
+# SVN-REVISION in the build tree.
+
+DOCKER=`which docker`
+if [ "X$DOCKER" == X ]; then
+  echo "Docker not on PATH."
+  exit 1
+fi
+
+CID=buildrbinaries
+
+USRDIR="/usr/lib/mxe/usr_aarch64"
+TCCFILE=`ls -1 llvm17_ucrt3_cross_aarch64_[0-9]*.tar.zst | head -1`
+MXETARGET="aarch64-w64-mingw32.static.posix"
+
+if [ "X$TCCFILE" == X ] || [ ! -r $TCCFILE ] ; then
+  echo "No cross-toolchain archive." >&2
+  exit 1
+fi
+
+TLREV=`echo $TCCFILE | sed -e 's/.*_\([0-9]\+\).tar.zst/\1/g'`
+TCLFILE=`ls -1 llvm17_ucrt3_base_aarch64_$TLREV.tar.zst | head -1`
+
+if [ ! -r $TCLFILE ] ; then
+  echo "No (matching) toolchain libraries archive." >&2
+  exit 1
+fi
+
+TCBFILE=`ls -1 Tcl-aarch64*zip | head -1`
+
+if [ "X$TCBFILE" == X ] || [ ! -r $TCBFILE ] ; then
+  echo "No Tcl/Tk bundle." >&2
+  exit 1
+fi
+
+X=`docker container ls -a | sed -e 's/.* //g' | grep -v NAMES | grep '^'$CID'$'`
+NEEDTL=yes
+
+if [ "X$X" != X$CID ] ; then
+  echo "Creating container $CID"
+  docker create --name $CID -it ubuntu:22.04
+  docker start $CID
+    
+  cat <<EOF | docker exec --interactive $CID bash -x
+  sed -i 's/^# deb-src/deb-src/g' /etc/apt/sources.list
+  apt-get update
+  echo "Europe/Prague" > /etc/timezone
+  env DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+  apt-get -y build-dep r-base
+  apt-get -y install rsync subversion libpcre2-dev zstd unzip
+  mkdir -p $USRDIR
+  cd /root
+EOF
+
+else
+  echo "Reusing container $CID"
+  
+  docker stop $CID
+  rm -f .version
+  docker cp $CID:$USRDIR/$MXETARGET/.version .
+  if [ -r .version ] && [ X`cat .version` == X$TLREV ] ; then
+    echo "Reusing toolchain in $CID"
+    NEEDTL=no
+  else
+    echo "Removing old toolchain in $CID"
+    docker start $CID
+    cat <<EOF | docker exec --interactive $CID bash -x
+    rm -rf $USRDIR
+    mkdir -p $USRDIR
+EOF
+    docker stop $CID
+  fi
+  rm -f .version
+  docker start $CID
+fi
+
+if [ $NEEDTL == yes ] ; then
+  echo "Installing toolchain into $CID"
+  
+  docker stop $CID
+  docker cp $TCCFILE $CID:$USRDIR
+  docker cp $TCLFILE $CID:$USRDIR
+  docker start $CID
+  
+  cat <<EOF | docker exec --interactive $CID bash -x
+  cd $USRDIR
+  tar xf $TCCFILE
+  tar xf $TCLFILE
+  rm -f $TCCFILE $TCLFILE
+EOF
+fi
+
+docker stop $CID
+docker cp cross_build_binaries.sh $CID:/root
+docker cp $TCBFILE $CID:/root
+docker start $CID
+
+cat <<EOF | docker exec --interactive $CID bash -x
+  cd /root
+  rm -rf cross *.out
+  bash -x ./cross_build_binaries.sh $TGT 2>&1 | tee cross_build.out
+EOF
+
+docker stop $CID
+
+rm -rf cross cross_build.out
+
+docker cp $CID:/root/cross_build.out cross_build.out
+docker cp $CID:/root/cross .
