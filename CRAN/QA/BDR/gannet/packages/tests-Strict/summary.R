06cb0fd00d7fb1c445a826a5be0054c2e284a901
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2024-08-18 10:39:31 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6273 c52295ea-58df-0310-926a-d16021944841

---
+++ CRAN/QA/BDR/gannet/packages/tests-Strict/summary.R
@@ -0,0 +1,16 @@
+options(warn = 1L)
+dest <- "/data/ftp/pub/bdr/Strict"
+files <- dir(patt = "[.]out$")
+ex <- c(RPANDA", "Rserve", "arcbpf", "mmocore", "mvMORPH")
+file <- setdiff(files, paste0(ex, ".out"))
+#patt <- "^ERROR: "
+patt <- "^ERROR: compilation failed for package"
+#patt <- "error: (implicit declaration|'PI' undeclared|.* was not declared in this scope|.* a declaration of .*must be available)"
+for(f in files) {
+   d <- file.path(dest, f)
+   if(any(grepl(patt, readLines(f), useBytes = TRUE)))
+	file.copy(f, dest, overwrite = TRUE, copy.date = TRUE)
+   else if (file.exists(d)) file.remove(d)
+}
+
+
