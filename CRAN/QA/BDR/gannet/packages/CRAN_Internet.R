61d95930bdfd7d89e8c990c28885e58b1913546a
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2024-09-23 17:56:10 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6311 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/CRAN_Internet.R
+++ CRAN/QA/BDR/gannet/packages/CRAN_Internet.R
@@ -113,7 +113,7 @@ function(packages, cran = TRUE, verbose = TRUE, before = NULL,
               else
                   as.Date(before)
     ## for shutdowns
-    before <- max(Sys.Date() + 14, as.Date("2024-08-19"))
+    before <- max(Sys.Date() + 14, as.Date("2024-09-11"))
 
     fmt <- c("Dear maintainer,",
              "",
