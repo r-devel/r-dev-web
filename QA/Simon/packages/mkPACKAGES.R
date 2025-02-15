c8b3800a0bc516a7487a5f114cc6763d59de7794
urbaneks <urbaneks@c52295ea-58df-0310-926a-d16021944841>
2025-02-15 09:52:52 +0000

    mkPACKAGES.R: fix NeedsCompilation, MD5sum and add SHA256sum
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6434 c52295ea-58df-0310-926a-d16021944841

--- QA/Simon/packages/mkPACKAGES.R
+++ QA/Simon/packages/mkPACKAGES.R
@@ -2,21 +2,22 @@ ver = Sys.getenv("VER")
 if (!isTRUE(nzchar(ver))) ver <- "4.1"
 fn = Sys.glob(file.path("bin",ver,"*.DESCRIPTION"))
 type <- "mac.binary"
-       fields <- unique(c(tools:::.get_standard_repository_db_fields(type), NULL))
+fields <- unique(c(tools:::.get_standard_repository_db_fields(type), "SHA256sum", NULL))
 l = parallel::mclapply(fn, function(p) {
 ## tools/R/packages.R L202:
                 temp <- tryCatch(read.dcf(p, fields = fields)[1L, ],
                                  error = identity)
 
                 if(!inherits(temp, "error")) {
-                    if("NeedsCompilation" %in% fields &&
+                    if(!"NeedsCompilation" %in% fields ||
                        is.na(temp["NeedsCompilation"])) {
-		       if (length(grep("/src/",readLines(gsub(p, "DESCRIPTION$", "content")))))
                         temp["NeedsCompilation"] <-
-                            if(length(grep("/src/",readLines(gsub(p, "DESCRIPTION$", "content"))))) "yes" else "no"
+                            if(length(grep("/libs/",readLines(gsub("DESCRIPTION$", "content", p))))) "yes" else "no"
 	            }
-		    md5 <- tryCatch(readLines(gsub(p, "DESCRIPTION$", "MD5")), error=function(e) NULL)
+		    md5 <- tryCatch(gsub(".*= ","",readLines(gsub("DESCRIPTION$", "MD5", p))), error=function(e) NULL)
+		    sha256 <- tryCatch(gsub(".*= ","",readLines(gsub("DESCRIPTION$", "SHA256", p))), error=function(e) NULL)
                     if (length(md5)) temp["MD5sum"] <- md5[1L]
+                    if (length(sha256)) temp["SHA256sum"] <- sha256[1L]
                     temp
                 } else {
                     message(gettextf("reading DESCRIPTION for package %s failed with message:\n  %s",
