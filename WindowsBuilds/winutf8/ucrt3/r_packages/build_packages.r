3e42b0131c66fe0bf7150a9828e954cc64f481b2
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2025-03-06 10:10:03 +0000

    Increase timeout.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6522 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/r_packages/build_packages.r
+++ WindowsBuilds/winutf8/ucrt3/r_packages/build_packages.r
@@ -86,7 +86,7 @@ mkdir <- function(d)
 mkdir("pkgbuild/lib")
 owd <- setwd("pkgbuild")
 
-Sys.setenv("_R_INSTALL_PACKAGES_ELAPSED_TIMEOUT_"=3000)
+Sys.setenv("_R_INSTALL_PACKAGES_ELAPSED_TIMEOUT_"=4000)
 
 install.packages(pkgs=toinst, contriburl=contriburl, Ncpus=Ncpus, 
   keep_outputs=TRUE, type="source", lib="lib", available=ap,
