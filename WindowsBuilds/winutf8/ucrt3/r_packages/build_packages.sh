5c7b6f83e055bf523c6ab8294720e19ab2fba729
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2025-05-28 17:58:16 +0000

    Cmake gets confused by a symlink.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6610 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/r_packages/build_packages.sh
+++ WindowsBuilds/winutf8/ucrt3/r_packages/build_packages.sh
@@ -231,7 +231,7 @@ mkdir -p c:/rtools45/usr/bin
 
 export PATH="`pwd`/rinst/bin/:`pwd`/rinst/Tcl/bin/:${PATH}"
 export _R_INSTALL_TIME_PATCHES_=`pwd`
-export R_CUSTOM_TOOLS_SOFT=`pwd`/$TDIR
+export R_CUSTOM_TOOLS_SOFT=$(realpath $(pwd)/$TDIR)
   # intentionally non-existent directory as using Msys2 which is on PATH
 export R_CUSTOM_TOOLS_PATH=custom_rtools
 export PATH="${R_CUSTOM_TOOLS_SOFT}/bin:${PATH}"
