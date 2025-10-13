233ea8b7a9b15c407e992114b9e523964259849b
kalibera <kalibera@c52295ea-58df-0310-926a-d16021944841>
2025-10-13 11:24:10 +0000

    Update package.
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6662 c52295ea-58df-0310-926a-d16021944841

--- WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/libarchive.mk
+++ WindowsBuilds/winutf8/ucrt3/toolchain_libs/mxe/src/libarchive.mk
@@ -4,22 +4,24 @@ PKG             := libarchive
 $(PKG)_WEBSITE  := https://www.libarchive.org/
 $(PKG)_DESCR    := Libarchive
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.8.0
-$(PKG)_CHECKSUM := 191b5b24811499d5c2e5efa3248975fa6daa5e6a227700cc7b8e54d6d7c06eef
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.libarchive.org/downloads/$($(PKG)_FILE)
+$(PKG)_VERSION  := 3.8.1
+$(PKG)_CHECKSUM := ad5dfe0d9a5999494c445b9bc3271d84160950bb1790cf892fb132f563d036b6
+#$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+#$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+#$(PKG)_URL      := https://www.libarchive.org/downloads/$($(PKG)_FILE)
+$(PKG)_GH_CONF  := libarchive/libarchive/tags, v
 $(PKG)_DEPS     := cc bzip2 libiconv libxml2 nettle openssl xz zlib
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.libarchive.org/downloads/' | \
-    $(SED) -n 's,.*libarchive-\([0-9][^<]*\)\.tar.*,\1,p' | \
-    $(SORT) -V | \
-    tail -1
-endef
+#define $(PKG)_UPDATE
+#    $(WGET) -q -O- 'https://www.libarchive.org/downloads/' | \
+#    $(SED) -n 's,.*libarchive-\([0-9][^<]*\)\.tar.*,\1,p' | \
+#    $(SORT) -V | \
+#    tail -1
+#endef
 
 define $(PKG)_BUILD
     # use nettle instead of bcrypt for CNG(Crypto Next Generation)
+    cd '$(SOURCE_DIR)' && autoreconf -fi
     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
         $(MXE_CONFIGURE_OPTS) \
         --disable-bsdtar \
