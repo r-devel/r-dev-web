9d508cd97fc0597b7eb9bc7a3621f584ee89ac8c
ripley <ripley@c52295ea-58df-0310-926a-d16021944841>
2025-11-29 21:21:42 +0000

    up
    git-svn-id: https://svn.r-project.org/R-dev-web/trunk@6726 c52295ea-58df-0310-926a-d16021944841

--- CRAN/QA/BDR/gannet/packages/Before.R
+++ CRAN/QA/BDR/gannet/packages/Before.R
@@ -1,2 +1,6 @@
-Before <- function() Sys.Date() + 14
-
+Before <- function(d = 14) 
+{
+    b = Sys.Date() + d
+    if(b >= as.Date("2025-12-23") && b < as.Date("2026-01-15")) b = as.Date("2026-01-15")
+    b
+}
